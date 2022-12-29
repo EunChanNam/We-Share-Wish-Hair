ea9518cd68dd76b1888bb70a7c9124e2f5be7ab3
남은찬 <eunchan@nam-eunchan-ui-MacBookAir.local>
2022-12-29 16:55:11 +0900

    initial commit

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/WeShareWishHairApplication.java
@@ -0,0 +1,13 @@
+package com.inq.wishhair.wesharewishhair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeShareWishHairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeShareWishHairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inq/wishhair/wesharewishhair/WeShareWishHairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inq.wishhair.wesharewishhair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeShareWishHairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
