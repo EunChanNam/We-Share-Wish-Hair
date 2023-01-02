e5cb0120636037825b7a4c651536e74e99b94d7c
EunChanNam <namhm23@gmail.com>
2023-01-02 13:00:11 +0900

    [Dev] 생성 시간을 저장하는 BaseEntity 구현

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/auditing/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.inq.wishhair.wesharewishhair.domain.auditing;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+}
