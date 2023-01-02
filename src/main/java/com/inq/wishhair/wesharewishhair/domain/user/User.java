149de6bcde4454ed291e5de0e71669a4bef77810
EunChanNam <namhm23@gmail.com>
2023-01-02 11:51:45 +0900

    [Edit] User 테이블에 NoArgsConstructor 추가, Sex enum 단순화

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/user/User.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/user/User.java
@@ -2,11 +2,14 @@ package com.inq.wishhair.wesharewishhair.domain.user;
 
 import com.inq.wishhair.wesharewishhair.domain.user.enums.Sex;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/inq/wishhair/wesharewishhair/domain/user/enums/Sex.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/user/enums/Sex.java
@@ -1,9 +1,6 @@
 package com.inq.wishhair.wesharewishhair.domain.user.enums;
 
 public enum Sex {
-    M("Man"),
-    W("Woman");
-
-    Sex(String value) {
-    }
+    MAN,
+    WOMAN
 }
