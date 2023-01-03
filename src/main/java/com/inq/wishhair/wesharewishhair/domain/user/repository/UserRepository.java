86bea6c5d7e2944b4643b89a3797f7f7b7141287
EunChanNam <namhm23@gmail.com>
2023-01-03 13:19:28 +0900

    [Dev] UserService - 회원가입 로직 작성

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/user/repository/UserRepository.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/user/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.inq.wishhair.wesharewishhair.domain.user.repository;
 import com.inq.wishhair.wesharewishhair.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/inq/wishhair/wesharewishhair/domain/user/service/UserService.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/user/service/UserService.java
@@ -1,7 +1,18 @@
 package com.inq.wishhair.wesharewishhair.domain.user.service;
 
+import com.inq.wishhair.wesharewishhair.domain.user.User;
+import com.inq.wishhair.wesharewishhair.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Long createUser(User user) {
+        User saveUser = userRepository.save(user);
+        return saveUser.getId();
+    }
 }
