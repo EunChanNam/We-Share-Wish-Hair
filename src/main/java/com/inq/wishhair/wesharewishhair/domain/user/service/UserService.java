d898d61feffdf840d06a3b568164be60f82adf66
EunChanNam <namhm23@gmail.com>
2023-01-03 13:09:45 +0900

    [Dev] Repository, Service, Controller 생성

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/user/service/UserService.java
@@ -0,0 +1,7 @@
+package com.inq.wishhair.wesharewishhair.domain.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
---
+++ src/main/java/com/inq/wishhair/wesharewishhair/web/user/UserController.java
@@ -0,0 +1,7 @@
+package com.inq.wishhair.wesharewishhair.web.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
