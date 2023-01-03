b33f23fd0d69d8ba27aaa0642ddaa0ba1e65c6ed
EunChanNam <namhm23@gmail.com>
2023-01-03 16:35:55 +0900

    [Dev] ErrorResponse, ApiExcpetionHandler 구현

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/web/ApiExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.inq.wishhair.wesharewishhair.web;
+
+import com.inq.wishhair.wesharewishhair.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.exception.ErrorResponse;
+import com.inq.wishhair.wesharewishhair.exception.WishHairException;
+import com.inq.wishhair.wesharewishhair.web.user.UserController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(assignableTypes = {
+        UserController.class
+})
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(WishHairException.class)
+    public ResponseEntity<ErrorResponse> handleException(WishHairException e) {
+
+        ErrorCode errorCode = e.getErrorCode();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(new ErrorResponse(errorCode));
+        )
+    }
+}
