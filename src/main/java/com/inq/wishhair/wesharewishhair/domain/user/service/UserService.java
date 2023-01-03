41de29351740e58c6b3c65a0c1cab32aca9baf05
EunChanNam <namhm23@gmail.com>
2023-01-03 13:20:31 +0900

    [Fix] UserService - 회원가입 로직에 Transactional 추가

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/user/service/UserService.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/user/service/UserService.java
@@ -4,13 +4,16 @@ import com.inq.wishhair.wesharewishhair.domain.user.User;
 import com.inq.wishhair.wesharewishhair.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional
     public Long createUser(User user) {
         User saveUser = userRepository.save(user);
         return saveUser.getId();
