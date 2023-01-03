9fb7ebcd8da39495265561e110fa45ee6425a459
EunChanNam <namhm23@gmail.com>
2023-01-03 16:39:35 +0900

    [Dev] LoginService - login 로직 작성

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/login/LoginService.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/login/LoginService.java
@@ -1,6 +1,10 @@
 package com.inq.wishhair.wesharewishhair.domain.login;
 
+import com.inq.wishhair.wesharewishhair.domain.login.dto.UserSessionDto;
+import com.inq.wishhair.wesharewishhair.domain.user.User;
 import com.inq.wishhair.wesharewishhair.domain.user.repository.UserRepository;
+import com.inq.wishhair.wesharewishhair.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.exception.WishHairException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class LoginService {
 
     private final UserRepository userRepository;
+
+    public UserSessionDto login(String loginId, String pw) {
+
+        User findUser = userRepository.findByLoginId(loginId)
+                .filter(user -> user.getPw().equals(pw))
+                .orElseThrow(() -> new WishHairException(ErrorCode.LOGIN_FAIL));
+
+        return new UserSessionDto(findUser);
+    }
 }
