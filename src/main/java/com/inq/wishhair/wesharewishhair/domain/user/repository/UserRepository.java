5ca6aa026c642fa77cf7cdd7d310628df4dba233
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-01-04 12:07:35 +0900

    Revert "[BE] 로그인 구현 | -> BE (#5)"
    This reverts commit 00f8f4da0608b88ef563fd4710234a635c23d282.

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/user/repository/UserRepository.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/user/repository/UserRepository.java
@@ -3,9 +3,5 @@ package com.inq.wishhair.wesharewishhair.domain.user.repository;
 import com.inq.wishhair.wesharewishhair.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByLoginId(String loginId);
 }
