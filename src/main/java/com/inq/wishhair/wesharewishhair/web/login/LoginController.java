b637f36e9bce79f474d6ccb30cf5f6de6c8a7db2
EunChanNam <namhm23@gmail.com>
2023-01-04 12:21:23 +0900

    [Dev] 로그인시 Session에 sessionDto 저장

--- src/main/java/com/inq/wishhair/wesharewishhair/web/login/LoginController.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/web/login/LoginController.java
@@ -1,7 +1,11 @@
 package com.inq.wishhair.wesharewishhair.web.login;
 
 import com.inq.wishhair.wesharewishhair.domain.login.LoginService;
+import com.inq.wishhair.wesharewishhair.domain.login.dto.UserSessionDto;
+import com.inq.wishhair.wesharewishhair.web.common.consts.SessionConst;
 import com.inq.wishhair.wesharewishhair.web.login.dto.LoginRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +20,12 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(LoginRequest loginRequest) {
+    public ResponseEntity<Void> login(LoginRequest loginRequest, HttpServletRequest request) {
 
-        loginService.login(loginRequest.getLoginId(), loginRequest.getPw());
+        UserSessionDto sessionDto = loginService.login(loginRequest.getLoginId(), loginRequest.getPw());
+        HttpSession session = request.getSession(true);
+        session.setAttribute(SessionConst.LONGIN_MEMBER, sessionDto);
+
         return ResponseEntity.noContent().build();
     }
 }
