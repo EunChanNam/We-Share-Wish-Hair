7672e768bf4e994527810d08cba28e25e6fd1d82
EunChanNam <namhm23@gmail.com>
2023-01-05 12:09:02 +0900

    [Dev] HairStyleService - 헤어추천 로직 작성

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/hairstyle/service/HairStyleService.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/hairstyle/service/HairStyleService.java
@@ -1,7 +1,23 @@
 package com.inq.wishhair.wesharewishhair.domain.hairstyle.service;
 
+import com.inq.wishhair.wesharewishhair.domain.hairstyle.HairStyle;
+import com.inq.wishhair.wesharewishhair.domain.hairstyle.repository.HairStyleRepository;
+import com.inq.wishhair.wesharewishhair.domain.hashtag.enums.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class HairStyleService {
+
+    private final HairStyleRepository hairStyleRepository;
+
+    public List<HairStyle> findRecommendedHairStyle(List<Tag> tags) {
+
+        return hairStyleRepository.findByHashTags(tags, tags.size());
+    }
 }
