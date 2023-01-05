29ec0d75e1a4e4ed23947e4ac8ae9a946edf4535
EunChanNam <namhm23@gmail.com>
2023-01-05 13:54:28 +0900

    [Test] 헤어추천 쿼리 테스트

--- src/test/java/com/inq/wishhair/wesharewishhair/domain/hairstyle/HairStyleRepositoryTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/domain/hairstyle/HairStyleRepositoryTest.java
@@ -2,9 +2,12 @@ package com.inq.wishhair.wesharewishhair.domain.hairstyle;
 
 import com.inq.wishhair.wesharewishhair.domain.hairstyle.repository.HairStyleRepository;
 import com.inq.wishhair.wesharewishhair.domain.hashtag.enums.Tag;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
@@ -27,5 +30,7 @@ class HairStyleRepositoryTest {
 
         List<HairStyle> hairStyles = hairStyleRepository.findByHashTags(tags, tags.size());
         assertThat(hairStyles.get(0).getName()).isEqualTo("헤일로 펌");
+        assertThat(hairStyles.size()).isEqualTo(1);
+        assertThat(hairStyles.get(0).getPhotos().size()).isEqualTo(4);
     }
 }
