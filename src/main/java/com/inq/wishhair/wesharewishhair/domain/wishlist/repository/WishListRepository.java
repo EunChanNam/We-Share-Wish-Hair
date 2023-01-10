7eaff8f0e9c04ec24c00fc67dc0df5574a9f3f6c
EunChanNam <namhm23@gmail.com>
2023-01-10 14:34:53 +0900

    [Dev] WishListRespository - 찜목록 조회 로직 작성(2)

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/wishlist/repository/WishListRepository.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/wishlist/repository/WishListRepository.java
@@ -2,6 +2,15 @@ package com.inq.wishhair.wesharewishhair.domain.wishlist.repository;
 
 import com.inq.wishhair.wesharewishhair.domain.wishlist.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface WishListRepository extends JpaRepository<WishList, Long> {
+
+    @Query("select w from WishList w " +
+            "where w.user.id = :userId " +
+            "order by w.createdDate desc")
+    public List<WishList> findByUser(@Param("userId") Long userId);
 }
