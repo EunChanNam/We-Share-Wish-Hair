987b543db1a7fa671c64b59a8d5d713bf13d5c01
EunChanNam <namhm23@gmail.com>
2023-01-10 13:22:24 +0900

    [Dev] WishHair Entity에 user 속성 추가

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/wishlist/WishList.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/wishlist/WishList.java
@@ -1,6 +1,7 @@
 package com.inq.wishhair.wesharewishhair.domain.wishlist;
 
 import com.inq.wishhair.wesharewishhair.domain.hairstyle.HairStyle;
+import com.inq.wishhair.wesharewishhair.domain.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -11,10 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WishList {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hair_style_id")
     private HairStyle hairStyle;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
