c1b7ac39d960f7eb3b1df708646c3da2e960d6ab
EunChanNam <namhm23@gmail.com>
2023-01-10 13:19:15 +0900

    [Dev] WishList Entity 구현

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/wishlist/WishList.java
@@ -0,0 +1,20 @@
+package com.inq.wishhair.wesharewishhair.domain.wishlist;
+
+import com.inq.wishhair.wesharewishhair.domain.hairstyle.HairStyle;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WishList {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hair_style_id")
+    private HairStyle hairStyle;
+}
