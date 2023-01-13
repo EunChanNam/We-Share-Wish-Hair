5da905aba70e42429e01a97a99b3329daa180af5
rnawslgk <hamo35301@gmail.com>
2023-01-13 17:01:42 +0900

    fragment_2 RecommendFragment mockup

--- app/src/main/java/com/example/wishhair/RecommendFragment.java
+++ app/src/main/java/com/example/wishhair/RecommendFragment.java
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RecommendFragment extends Fragment {
+
+    TextView topBarTextView;
+    Button btn_submitPicture, btn_start;
 
     public RecommendFragment() {
         // Required empty public constructor
@@ -20,8 +24,12 @@ public class RecommendFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_recommend, container, false);
 
-        TextView topBarTextView = (TextView) v.findViewById(R.id.topBarTextView);
+        topBarTextView = v.findViewById(R.id.topBarTextView);
         topBarTextView.setText("AI 추천받기");
+
+        btn_submitPicture = v.findViewById(R.id.reco_btn_submitPicture);
+
+        btn_start = v.findViewById(R.id.reco_btn_start);
 
         return v;
     }
