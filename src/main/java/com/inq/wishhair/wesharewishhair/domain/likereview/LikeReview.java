fcfdee042e69a9ff14185cb9583c743c5aa3d22e
EunChanNam <namhm23@gmail.com>
2023-01-16 12:38:16 +0900

    [Dev] LikeReview Entity, Repository 생성

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/likereview/LikeReview.java
@@ -0,0 +1,26 @@
+package com.inq.wishhair.wesharewishhair.domain.likereview;
+
+import com.inq.wishhair.wesharewishhair.domain.review.Review;
+import com.inq.wishhair.wesharewishhair.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LikeReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+}
---
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/likereview/repository/LikeReviewRepository.java
@@ -0,0 +1,8 @@
+package com.inq.wishhair.wesharewishhair.domain.likereview.repository;
+
+import com.inq.wishhair.wesharewishhair.domain.likereview.LikeReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeReviewRepository extends JpaRepository<LikeReview, Long> {
+
+}
