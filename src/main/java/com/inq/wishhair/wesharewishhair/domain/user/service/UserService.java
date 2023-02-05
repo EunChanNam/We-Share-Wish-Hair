f8f6a9586f886594364d594a1306b20af272c1ad
EunChanNam <namhm23@gmail.com>
2023-02-05 15:25:59 +0900

    [Dev] UserService - 회원가입시 Join PointHistory 생성

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/user/service/UserService.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.inq.wishhair.wesharewishhair.domain.user.service;
 
+import com.inq.wishhair.wesharewishhair.domain.point.PointHistory;
+import com.inq.wishhair.wesharewishhair.domain.point.repository.PointHistoryRepository;
 import com.inq.wishhair.wesharewishhair.domain.user.User;
 import com.inq.wishhair.wesharewishhair.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PointHistoryRepository pointHistoryRepository;
 
     @Transactional
     public Long createUser(User user) {
         User saveUser = userRepository.save(user);
+        PointHistory joinPointHistory = PointHistory.createJoinPointHistory(saveUser);
+        pointHistoryRepository.save(joinPointHistory);
+
         return saveUser.getId();
     }
 }
