9951d57a24abbd69002e203949297c3d43bd075e
EunChanNam <namhm23@gmail.com>
2023-02-05 14:43:44 +0900

    [Dev] PointHistoryRepository - findRecentPointByUserId 작성

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/point/repository/PointHistoryRepository.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/point/repository/PointHistoryRepository.java
@@ -1,8 +1,18 @@
 package com.inq.wishhair.wesharewishhair.domain.point.repository;
 
 import com.inq.wishhair.wesharewishhair.domain.point.PointHistory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
 
+    @Query("select p from PointHistory p " +
+            "where p.user.id = :userId " +
+            "order by p.createdDate desc")
+    Optional<PointHistory> findRecentPointByUserId(@Param("userId") Long userId,
+                                                   Pageable pageable);
 }
