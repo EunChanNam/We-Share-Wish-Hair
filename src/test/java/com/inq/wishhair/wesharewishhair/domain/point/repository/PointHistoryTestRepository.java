780ff753f64d8c8c903b0d715a535e4d81ed41cb
EunChanNam <namhm23@gmail.com>
2023-03-10 15:29:39 +0900

    [Setting + Test] 테스트용 PointHistoryTestRepository 작성 + UserService - createUser Test code 작성

---
+++ src/test/java/com/inq/wishhair/wesharewishhair/domain/point/repository/PointHistoryTestRepository.java
@@ -0,0 +1,16 @@
+package com.inq.wishhair.wesharewishhair.domain.point.repository;
+
+import com.inq.wishhair.wesharewishhair.domain.point.PointHistory;
+import com.inq.wishhair.wesharewishhair.domain.user.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PointHistoryTestRepository extends JpaRepository<PointHistory, Long> {
+
+    @EntityGraph(attributePaths = {"user"})
+    Optional<PointHistory> findTopByUser(User user);
+}
--- src/test/java/com/inq/wishhair/wesharewishhair/domain/user/service/UserServiceTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/domain/user/service/UserServiceTest.java
@@ -1,30 +1,55 @@
 package com.inq.wishhair.wesharewishhair.domain.user.service;
 
 import com.inq.wishhair.wesharewishhair.WeShareWishHairApplication;
+import com.inq.wishhair.wesharewishhair.domain.point.PointHistory;
+import com.inq.wishhair.wesharewishhair.domain.point.repository.PointHistoryTestRepository;
 import com.inq.wishhair.wesharewishhair.domain.user.User;
-import com.inq.wishhair.wesharewishhair.domain.user.repository.UserRepository;
+import com.inq.wishhair.wesharewishhair.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.exception.WishHairException;
 import com.inq.wishhair.wesharewishhair.fixture.UserFixture;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestConstructor;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-@SpringBootTest
+@SpringBootTest(classes = {WeShareWishHairApplication.class, PointHistoryTestRepository.class})
 @DisplayName("UserServiceTest - SpringBootTest")
-//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class UserServiceTest {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PointHistoryTestRepository pointHistoryTestRepository;
+
     @Test
     @DisplayName("회원가입 서비스 로직 테스트")
     void createUserTest() {
+        User userA = UserFixture.A.toEntity();
 
+        //createUser 를 실행하기 전에는 PointHistory 가 존재하지 않는다.
+        assertThat(pointHistoryTestRepository.findAll()).isEmpty();
+
+        //logic
+        Long resultA = userService.createUser(userA);
+
+        PointHistory pointHistory = findPointHistoryByUser(userA);
+
+        //createUser 실행 후 PointHistory 가 생성된다.
+        assertThat(pointHistoryTestRepository.findAll()).isNotEmpty();
+
+        assertAll(
+                () -> assertThat(resultA).isEqualTo(userA.getId()), // createUser response validate
+                () -> assertThat(pointHistory.getUser().getId()).isEqualTo(userA.getId()) // pointHistory create validate
+        );
+
+    }
+
+    private PointHistory findPointHistoryByUser(User userA) {
+        return pointHistoryTestRepository.findTopByUser(userA)
+                .orElseThrow(() -> new WishHairException(ErrorCode.NOT_EXIST_KEY));
     }
 }
