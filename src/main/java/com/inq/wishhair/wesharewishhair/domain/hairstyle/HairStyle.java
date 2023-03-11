ba0f0aa0d8a6160cee84fdf5583bc95a02bbbe9a
EunChanNam <namhm23@gmail.com>
2023-03-11 21:52:35 +0900

    [Dev] HashTagRepository 생성

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/hairstyle/HairStyle.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/hairstyle/HairStyle.java
@@ -30,5 +30,4 @@ public class HairStyle {
 
     @Enumerated(EnumType.STRING)
     private Sex sex;
-
 }
---
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/hashtag/repository/HashTagRepository.java
@@ -0,0 +1,7 @@
+package com.inq.wishhair.wesharewishhair.domain.hashtag.repository;
+
+import com.inq.wishhair.wesharewishhair.domain.hashtag.HashTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashTagRepository extends JpaRepository<HashTag, Long> {
+}
