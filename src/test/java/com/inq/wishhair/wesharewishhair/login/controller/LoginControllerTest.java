1fb46708fe5257f4b5ecfc7b082dc2319af9202e
EunChanNam <namhm23@gmail.com>
2023-03-13 16:23:15 +0900

    [Test] LoginControllerTest - 올바르지 않은 아이디로 시도해 로그인에 실패한다

--- src/test/java/com/inq/wishhair/wesharewishhair/login/controller/LoginControllerTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/login/controller/LoginControllerTest.java
@@ -1,24 +1,19 @@
 package com.inq.wishhair.wesharewishhair.login.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.inq.wishhair.wesharewishhair.common.base.ControllerTest;
 import com.inq.wishhair.wesharewishhair.domain.login.dto.UserSessionDto;
 import com.inq.wishhair.wesharewishhair.domain.user.User;
+import com.inq.wishhair.wesharewishhair.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.exception.ErrorResponse;
+import com.inq.wishhair.wesharewishhair.exception.WishHairException;
 import com.inq.wishhair.wesharewishhair.fixture.UserFixture;
 import com.inq.wishhair.wesharewishhair.login.controller.utils.LoginRequestUtils;
 import com.inq.wishhair.wesharewishhair.web.login.dto.LoginRequest;
-import lombok.SneakyThrows;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -27,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("LoginControllerTest - WebMvcTest")
 public class LoginControllerTest extends ControllerTest {
 
-    private static String LOGIN_URL = "/api/login";
+    private static final String LOGIN_URL = "/api/login";
 
     @Test
     @DisplayName("올바른 로그인 아이디, 비밀번호를 통해 로그인을 성공한다.")
@@ -38,16 +33,42 @@ public class LoginControllerTest extends ControllerTest {
                 .willReturn(getSessionDto());
 
         //todo 세션에 등록되는 정보는 테스트를 따로 안해줘도 되는지
-
+        //when
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
                 .post(LOGIN_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(getJsonAsString(request));
+                .param("loginId", request.getLoginId())
+                .param("pw", request.getPw());
 
+        //then
         mockMvc.perform(requestBuilder)
                 .andExpectAll(
                         status().isNoContent(),
                         jsonPath("$").doesNotExist()
+                );
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 아이디로 시도해 로그인에 실패한다.")
+    void test2() throws Exception {
+        //given
+        ErrorCode expectedError = ErrorCode.LOGIN_FAIL;
+        LoginRequest request = LoginRequestUtils.createWrongLoginIdRequest();
+        given(loginService.login(request.getLoginId(), request.getPw()))
+                .willThrow(new WishHairException(expectedError));
+
+        //when
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post(LOGIN_URL)
+                .param("loginId", request.getLoginId())
+                .param("pw", request.getPw());
+
+        //then
+        mockMvc.perform(requestBuilder)
+                .andExpectAll(
+                        status().isBadRequest(),
+                        jsonPath("$").exists(),
+                        jsonPath("$.code").value(expectedError.getCode()),
+                        jsonPath("$.message").value(expectedError.getMessage())
                 );
     }
 
--- src/test/java/com/inq/wishhair/wesharewishhair/login/controller/utils/LoginRequestUtils.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/login/controller/utils/LoginRequestUtils.java
@@ -7,10 +7,18 @@ import com.inq.wishhair.wesharewishhair.web.login.dto.LoginRequest;
 public class LoginRequestUtils {
 
     private static final UserFixture A = UserFixture.A;
+    private static final String wrongLoginId = A.getLoginId() + "fail";
 
     public static LoginRequest createRequest() {
         return new LoginRequest(
                 A.getLoginId(),
+                A.getPw()
+        );
+    }
+
+    public static LoginRequest createWrongLoginIdRequest() {
+        return new LoginRequest(
+                wrongLoginId,
                 A.getPw()
         );
     }
