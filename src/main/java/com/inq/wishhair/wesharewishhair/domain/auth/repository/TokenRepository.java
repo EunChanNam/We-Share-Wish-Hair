5b03a25d3bb053b6e0441e45fa6ac2403999241d
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-03-15 13:35:38 +0900

    [로그인 방식 수정] Token-domain 작성 (#18)
    * [Dev] Token Entity 작성
    
    * TokenRepository 작성

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/auth/repository/TokenRepository.java
@@ -0,0 +1,22 @@
+package com.inq.wishhair.wesharewishhair.domain.auth.repository;
+
+import com.inq.wishhair.wesharewishhair.domain.auth.Token;
+import com.inq.wishhair.wesharewishhair.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    @Query("select t from Token t where t.user.id = :userId")
+    Optional<Token> findByUserId(@Param("userId") Long userId);
+
+    boolean existsByUser(User user);
+
+    @Query("delete from Token t where t.user.id = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
+}
