1efa7e96a3c0644a7bad1a0ae6d1ef708f6cb969
EunChanNam <namhm23@gmail.com>
2023-03-16 12:37:46 +0900

    [Dev] TokenReissueService - 재인증 로직에 RefreshToken 재발급 로직 추가

--- src/main/java/com/inq/wishhair/wesharewishhair/domain/auth/service/TokenReissueService.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/domain/auth/service/TokenReissueService.java
@@ -23,14 +23,16 @@ public class TokenReissueService {
             throw new WishHairException(ErrorCode.AUTH_INVALID_TOKEN);
         }
 
-        //access 토큰만 재발금
-        String newAccessToken = provider.createAccessToken(userId);
-
-        //refresh 토큰이 만료됐으면 새로운 Refresh 토큰 발급
+        //RefreshToken 이 만료됐으면 다시 로그인을 통해 인증하도록 에러던짐
         if (!provider.isValidToken(refreshToken)) {
-            refreshToken = provider.createRefreshToken(userId);
+            throw new WishHairException(ErrorCode.AUTH_EXPIRED_TOKEN);
         }
 
-        return TokenResponse.of(newAccessToken, refreshToken);
+        String newAccessToken = provider.createAccessToken(userId);
+        String newRefreshToken = provider.createRefreshToken(userId);
+
+        tokenManager.updateRefreshTokenByRTR(userId, newRefreshToken);
+
+        return TokenResponse.of(newAccessToken, newRefreshToken);
     }
 }
