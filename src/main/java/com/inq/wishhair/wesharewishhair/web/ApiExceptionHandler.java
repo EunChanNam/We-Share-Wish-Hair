66c0b007a1b6135d29bb1e9c929b3aab33d03e4d
EunChanNam <namhm23@gmail.com>
2023-03-17 02:16:45 +0900

    [Edit] ApiExceptionHandler 새로운 AuthController 등록

--- src/main/java/com/inq/wishhair/wesharewishhair/web/ApiExceptionHandler.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/web/ApiExceptionHandler.java
@@ -3,14 +3,18 @@ package com.inq.wishhair.wesharewishhair.web;
 import com.inq.wishhair.wesharewishhair.exception.ErrorCode;
 import com.inq.wishhair.wesharewishhair.exception.ErrorResponse;
 import com.inq.wishhair.wesharewishhair.exception.WishHairException;
-import com.inq.wishhair.wesharewishhair.web.login.LoginController;
+import com.inq.wishhair.wesharewishhair.web.auth.AuthController;
+import com.inq.wishhair.wesharewishhair.web.hairstyle.HairStyleController;
+import com.inq.wishhair.wesharewishhair.web.review.ReviewController;
 import com.inq.wishhair.wesharewishhair.web.user.UserController;
+import com.inq.wishhair.wesharewishhair.web.wishlist.WishListController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(assignableTypes = {
-        UserController.class, LoginController.class
+        UserController.class, AuthController.class, HairStyleController.class,
+        ReviewController.class, WishListController.class, AuthController.class
 })
 public class ApiExceptionHandler {
 
