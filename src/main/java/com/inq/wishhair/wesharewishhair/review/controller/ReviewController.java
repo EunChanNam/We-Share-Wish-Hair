4822fd9a1aa55e3eba1a164390802b166c01be7f
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-03-18 19:25:29 +0900

    [구조 리펙토링] wish-list 응답, 로직 구조 변경 + 모든 응답 제네릭 제거 (#46)
    * [Refactor] WishListResponse : controller -> service
    
    * [Edit] WishList 조회에 페이징 추가
    
    * [Refactor] PagedWishListResponse 정의
    
    * [Refactor] service - getWishList 반환값 WishListResponse 로 수정
    
    * [Refactor] PagedReviewResponse 제네릭 제거
    
    * [Refactor] Review - 조회 로직 분리
    
    * [Refactor] PagedHairStyleResponse - 제네릭 제거

--- src/main/java/com/inq/wishhair/wesharewishhair/review/controller/ReviewController.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/review/controller/ReviewController.java
@@ -1,18 +1,13 @@
 package com.inq.wishhair.wesharewishhair.review.controller;
 
 import com.inq.wishhair.wesharewishhair.auth.config.resolver.ExtractPayload;
-import com.inq.wishhair.wesharewishhair.review.controller.dto.response.PagedReviewResponse;
-import com.inq.wishhair.wesharewishhair.review.domain.Review;
 import com.inq.wishhair.wesharewishhair.review.service.ReviewService;
 import com.inq.wishhair.wesharewishhair.review.controller.dto.request.ReviewRequest;
-import com.inq.wishhair.wesharewishhair.review.service.dto.response.ReviewResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,18 +25,5 @@ public class ReviewController {
         return ResponseEntity
                 .created(URI.create("/api/review/" + reviewId))
                 .build();
-    }
-
-    @GetMapping("/review")
-    public ResponseEntity<PagedReviewResponse<List<ReviewResponse>>> getReviews(Pageable pageable,
-                                                           @RequestParam String condition) {
-
-        List<ReviewResponse> result = reviewService.getReviews(pageable, condition);
-
-        return ResponseEntity.ok(toPagedResponse(result, result.size()));
-    }
-
-    private <T> PagedReviewResponse<T> toPagedResponse(T result, int contentSize) {
-        return new PagedReviewResponse<>(result, contentSize);
     }
 }
---
+++ src/main/java/com/inq/wishhair/wesharewishhair/review/service/ReviewFindService.java
@@ -0,0 +1,39 @@
+package com.inq.wishhair.wesharewishhair.review.service;
+
+import com.inq.wishhair.wesharewishhair.global.consts.Condition;
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyleRepository;
+import com.inq.wishhair.wesharewishhair.photo.PhotoStore;
+import com.inq.wishhair.wesharewishhair.review.domain.Review;
+import com.inq.wishhair.wesharewishhair.review.domain.ReviewRepository;
+import com.inq.wishhair.wesharewishhair.review.service.dto.response.ReviewResponse;
+import com.inq.wishhair.wesharewishhair.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ReviewFindService {
+
+    private final ReviewRepository reviewRepository;
+
+    public List<ReviewResponse> getReviews(Pageable pageable, String condition) {
+        List<Review> reviews = reviewRepository.findReviewByPaging(pageable);
+        // Query 에서 정렬이 안돼서 Service 에서 정렬
+        if (condition.equals(Condition.LIKES)) {
+            reviews.sort((a, b) -> Integer.compare(b.getLikeReviews().size(), a.getLikeReviews().size()));
+        }
+
+        return toResponse(reviews);
+    }
+
+    private List<ReviewResponse> toResponse(List<Review> reviews) {
+        return reviews.stream()
+                .map(ReviewResponse::new)
+                .toList();
+    }
+}
