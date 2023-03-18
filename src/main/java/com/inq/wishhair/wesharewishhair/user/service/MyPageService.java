1fc7bf056ec2c403d850933546726e873320baf1
EunChanNam <namhm23@gmail.com>
2023-03-18 16:48:29 +0900

    [Refactor] MyPageController 구현

--- src/main/java/com/inq/wishhair/wesharewishhair/user/service/MyPageService.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/user/service/MyPageService.java
@@ -1,18 +1,11 @@
 package com.inq.wishhair.wesharewishhair.user.service;
 
-import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
-import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
 import com.inq.wishhair.wesharewishhair.user.controller.dto.response.MyPageResponse;
 import com.inq.wishhair.wesharewishhair.user.domain.User;
 import com.inq.wishhair.wesharewishhair.user.domain.point.domain.PointHistory;
-import com.inq.wishhair.wesharewishhair.user.domain.point.domain.PointRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
