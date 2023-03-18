525e715092587e822e9b7042e6d73eb628e53388
EunChanNam <namhm23@gmail.com>
2023-03-18 15:49:14 +0900

    [Edit] ReviewRespository - 컬렉션 패치조인에 distinct 추가

--- src/main/java/com/inq/wishhair/wesharewishhair/review/domain/ReviewRepository.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/review/domain/ReviewRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     //likeReviews 는 fetch join, photos 는 batch_fetch_size 로 해결
-    @Query("select r from Review r " +
+    @Query("select distinct r from Review r " +
             "left outer join fetch r.likeReviews " +
             "join fetch r.hairStyle " +
             "join fetch r.user")
