c43835b8de036c0b79d500629aedd9b636b915c0
EunChanNam <namhm23@gmail.com>
2023-03-20 01:27:18 +0900

    [Test] AuthControllerTest - 로그인에 실패하면 로그인 실패 400 예외를 던진다

--- src/test/java/com/inq/wishhair/wesharewishhair/auth/controller/AuthControllerTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/auth/controller/AuthControllerTest.java
@@ -3,6 +3,8 @@ package com.inq.wishhair.wesharewishhair.auth.controller;
 import com.inq.wishhair.wesharewishhair.auth.controller.dto.request.LoginRequest;
 import com.inq.wishhair.wesharewishhair.auth.controller.utils.LoginRequestUtil;
 import com.inq.wishhair.wesharewishhair.global.base.ControllerTest;
+import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static com.inq.wishhair.wesharewishhair.fixture.TokenFixture.A;
+import static com.inq.wishhair.wesharewishhair.global.exception.ErrorCode.LOGIN_FAIL;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,7 +26,7 @@ public class AuthControllerTest extends ControllerTest {
     @DisplayName("로그인 API")
     class login {
         @Test
-        @DisplayName("로그인에 성공한다")
+        @DisplayName("로그인에 성공하고 응답 토큰을 받는다")
         void test1() throws Exception {
             //given
             LoginRequest request = LoginRequestUtil.createRequest();
@@ -31,10 +34,7 @@ public class AuthControllerTest extends ControllerTest {
                     .willReturn(A.toTokenResponse());
 
             //when
-            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
-                    .post(LOGIN_URL)
-                    .param("loginId", request.getLoginId())
-                    .param("pw", request.getPw());
+            MockHttpServletRequestBuilder requestBuilder = buildLoginRequest(request);
 
             //then
             mockMvc.perform(requestBuilder)
@@ -47,5 +47,35 @@ public class AuthControllerTest extends ControllerTest {
                             jsonPath("$.refreshToken").value(A.getRefreshToken())
                     );
         }
+
+        @Test
+        @DisplayName("로그인에 실패하면 로그인 실패 400 예외를 던진다")
+        void test2() throws Exception {
+            //given
+            LoginRequest request = LoginRequestUtil.createRequest();
+            given(authService.login(request.getLoginId(), request.getPw()))
+                    .willThrow(new WishHairException(LOGIN_FAIL));
+
+            //when
+            MockHttpServletRequestBuilder requestBuilder = buildLoginRequest(request);
+
+            //then
+            mockMvc.perform(requestBuilder)
+                    .andExpectAll(
+                            status().isBadRequest(),
+                            jsonPath("$").exists(),
+                            jsonPath("$.code").exists(),
+                            jsonPath("$.code").value(LOGIN_FAIL.getCode()),
+                            jsonPath("$.message").exists(),
+                            jsonPath("$.message").value(LOGIN_FAIL.getMessage())
+                    );
+        }
+    }
+
+    private MockHttpServletRequestBuilder buildLoginRequest(LoginRequest request) {
+        return MockMvcRequestBuilders
+                .post(LOGIN_URL)
+                .param("loginId", request.getLoginId())
+                .param("pw", request.getPw());
     }
 }
