a6aa71b2360c758fd407ff9d5f2142fb17aebde4
EunChanNam <namhm23@gmail.com>
2023-03-19 23:45:35 +0900

    [Test] TokenReissueServiceTest - 잘못된 토큰으로 재발급에 실패한다

--- src/test/java/com/inq/wishhair/wesharewishhair/auth/service/TokenReissueServiceTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/auth/service/TokenReissueServiceTest.java
@@ -4,11 +4,14 @@ import com.inq.wishhair.wesharewishhair.auth.domain.Token;
 import com.inq.wishhair.wesharewishhair.auth.service.dto.response.TokenResponse;
 import com.inq.wishhair.wesharewishhair.fixture.UserFixture;
 import com.inq.wishhair.wesharewishhair.global.base.ServiceTest;
+import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
 import com.inq.wishhair.wesharewishhair.user.domain.User;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("TokenReissueServiceTest - SpringBootTest")
@@ -20,11 +23,13 @@ public class TokenReissueServiceTest extends ServiceTest {
     private TokenReissueService tokenReissueService;
 
     private final User user = UserFixture.A.toEntity();
+    private final Token originToken = Token.issue(user, REFRESH_TOKEN);
 
     @BeforeEach
     void setUp() {
         //given
         userRepository.save(user);
+        tokenRepository.save(originToken);
     }
 
     @Nested
@@ -33,10 +38,6 @@ public class TokenReissueServiceTest extends ServiceTest {
         @Test
         @DisplayName("성공적으로 재발급 받는다")
         void test1() {
-            //given
-            Token originToken = Token.issue(user, REFRESH_TOKEN);
-            tokenRepository.save(originToken);
-
             //when
             TokenResponse result = tokenReissueService.reissueToken(user.getId(), REFRESH_TOKEN);
 
@@ -45,6 +46,18 @@ public class TokenReissueServiceTest extends ServiceTest {
                     () -> assertThat(result).isNotNull(),
                     () -> assertThat(result.getRefreshToken()).isNotEqualTo(REFRESH_TOKEN)
             );
+        }
+
+        @Test
+        @DisplayName("잘못된 토큰으로 재발급에 실패한다")
+        void test2() {
+            //given
+            String wrongToken = REFRESH_TOKEN + "fail";
+
+            //when, then
+            assertThatThrownBy(() -> tokenReissueService.reissueToken(user.getId(), wrongToken))
+                    .isInstanceOf(WishHairException.class)
+                    .hasMessageContaining(ErrorCode.AUTH_INVALID_TOKEN.getMessage());
         }
     }
 }
