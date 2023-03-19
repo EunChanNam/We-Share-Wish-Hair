d554cdbd9b2b8ea95d8b5195326e5139775e8455
EunChanNam <namhm23@gmail.com>
2023-03-19 17:29:17 +0900

    [Setting] test 설정파일 ignore 적용

---
+++ src/test/java/com/inq/wishhair/wesharewishhair/auth/utils/JwtTokenProviderTest.java
@@ -0,0 +1,11 @@
+package com.inq.wishhair.wesharewishhair.auth.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("JwtTokenProviderTest")
+public class JwtTokenProviderTest {
+
+    @Test
+    @DisplayName("")
+}
