713294a69b1152d0bb82ce6124f34735a801cf27
EunChanNam <namhm23@gmail.com>
2023-03-19 21:37:48 +0900

    [Test] TokenManagerTest - user 에게 발급된 토큰이 있으면 기존 토큰을 업데이트한다

--- src/test/java/com/inq/wishhair/wesharewishhair/auth/service/TokenManagerTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/auth/service/TokenManagerTest.java
@@ -4,15 +4,13 @@ import com.inq.wishhair.wesharewishhair.auth.domain.Token;
 import com.inq.wishhair.wesharewishhair.fixture.UserFixture;
 import com.inq.wishhair.wesharewishhair.global.base.ServiceTest;
 import com.inq.wishhair.wesharewishhair.user.domain.User;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("TokenManagerTest - SpringBootTest")
 public class TokenManagerTest extends ServiceTest {
@@ -44,6 +42,29 @@ public class TokenManagerTest extends ServiceTest {
             //then
             Optional<Token> result = tokenRepository.findByUser(user);
             assertThat(result).isPresent();
+        }
+
+        @Test
+        @DisplayName("user 에게 발급된 토큰이 있으면 기존 토큰을 업데이트한다.")
+        void test2() {
+            //given
+            String refreshToken = provider.createRefreshToken(user.getId());
+            tokenRepository.save(Token.issue(user, refreshToken));
+            String newRefreshToken = refreshToken + "diff";
+
+            //when
+            tokenManager.synchronizeRefreshToken(user, newRefreshToken);
+
+            //then
+            Optional<Token> result = tokenRepository.findByUser(user);
+            assertAll(
+                    () -> assertThat(result).isPresent(),
+                    () -> {
+                        Token token = result.get();
+                        assertThat(token.getRefreshToken()).isEqualTo(newRefreshToken);
+                        assertThat(token.getRefreshToken()).isNotEqualTo(refreshToken);
+                    }
+            );
         }
     }
 }
