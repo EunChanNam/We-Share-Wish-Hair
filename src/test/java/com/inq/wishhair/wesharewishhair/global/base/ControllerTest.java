2d775303f4172204e18b544a9889c6500b276951
EunChanNam <namhm23@gmail.com>
2023-03-19 22:27:42 +0900

    [Fix] usercontrollertest - 의존성 주입 버그 픽스

--- src/test/java/com/inq/wishhair/wesharewishhair/global/base/ControllerTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/global/base/ControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(value = {UserController.class, HairStyleController.class})
+@WebMvcTest(value = {UserController.class, HairStyleController.class, JwtTokenProvider.class})
 public abstract class ControllerTest {
 
     @Autowired
