2664cc342afd6215d5af7432752cc7098f0ffcca
EunChanNam <namhm23@gmail.com>
2023-03-20 00:41:09 +0900

    [Setting] LoginRequestUtil 구현

---
+++ src/test/java/com/inq/wishhair/wesharewishhair/auth/controller/utils/LoginRequestUtil.java
@@ -0,0 +1,13 @@
+package com.inq.wishhair.wesharewishhair.auth.controller.utils;
+
+import com.inq.wishhair.wesharewishhair.auth.controller.dto.request.LoginRequest;
+
+public abstract class LoginRequestUtil {
+
+    private static final String loginId = "loginId";
+    private static final String pw = "pw";
+
+    public static LoginRequest createRequest() {
+        return new LoginRequest(loginId, pw);
+    }
+}
