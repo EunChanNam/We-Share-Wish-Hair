c04558ccb6e262e7fa3412198d39b1d4c131dbf1
EunChanNam <namhm23@gmail.com>
2023-03-19 23:00:42 +0900

    [Fix] TokenReissueService - Refresh토큰 검사 로직 제거

---
+++ src/test/java/com/inq/wishhair/wesharewishhair/auth/service/TokenReissueServiceTest.java
@@ -0,0 +1,14 @@
+package com.inq.wishhair.wesharewishhair.auth.service;
+
+import com.inq.wishhair.wesharewishhair.global.base.ServiceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@DisplayName("TokenReissueServiceTest - SpringBootTest")
+public class TokenReissueServiceTest extends ServiceTest {
+
+    @Autowired
+    private TokenReissueService tokenReissueService;
+
+
+}
