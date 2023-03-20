9454b431b6665077603d11aba47b3edea774244c
EunChanNam <namhm23@gmail.com>
2023-03-20 15:32:16 +0900

    [Test] TokenReissueControllerTest - 만료된 Refresh 토큰으로 재발급에 실패해 만료된 토큰 예외를 던진다

--- src/test/java/com/inq/wishhair/wesharewishhair/auth/controller/TokenReissueControllerTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/auth/controller/TokenReissueControllerTest.java
@@ -1,13 +1,16 @@
 package com.inq.wishhair.wesharewishhair.auth.controller;
 
+import com.inq.wishhair.wesharewishhair.auth.service.dto.response.TokenResponse;
 import com.inq.wishhair.wesharewishhair.global.base.ControllerTest;
 import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
+import com.inq.wishhair.wesharewishhair.global.utils.TokenUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.inq.wishhair.wesharewishhair.global.exception.ErrorCode.AUTH_EXPIRED_TOKEN;
 import static com.inq.wishhair.wesharewishhair.global.exception.ErrorCode.AUTH_INVALID_TOKEN;
 import static com.inq.wishhair.wesharewishhair.global.utils.TokenUtils.*;
 import static org.mockito.BDDMockito.given;
@@ -28,16 +31,12 @@ public class TokenReissueControllerTest extends ControllerTest {
         @DisplayName("잘못된 Refresh 토큰으로 재발급에 실패해 유효하지 않은 토큰 예외를 던진다")
         void test1() throws Exception {
             //given
-            given(provider.isValidToken(REFRESH_TOKEN)).willReturn(true);
-            given(provider.getId(REFRESH_TOKEN)).willReturn(1L);
-
+            setSuccessService();
             given(tokenReissueService.reissueToken(1L, REFRESH_TOKEN))
                     .willThrow(new WishHairException(AUTH_INVALID_TOKEN));
 
             //when
-            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
-                    .post(BASE_URL)
-                    .header(AUTHORIZATION, BEARER + REFRESH_TOKEN);
+            MockHttpServletRequestBuilder requestBuilder = buildReissueRequest();
 
             //then
             mockMvc.perform(requestBuilder)
@@ -46,5 +45,34 @@ public class TokenReissueControllerTest extends ControllerTest {
                             jsonPath("$.message").value(AUTH_INVALID_TOKEN.getMessage())
                     );
         }
+
+        @Test
+        @DisplayName("만료된 Refresh 토큰으로 재발급에 실패해 만료된 토큰 예외를 던진다")
+        void test2() throws Exception {
+            //given
+            given(provider.isValidToken(REFRESH_TOKEN))
+                    .willThrow(new WishHairException(AUTH_EXPIRED_TOKEN));
+
+            //when
+            MockHttpServletRequestBuilder requestBuilder = buildReissueRequest();
+
+            //then
+            mockMvc.perform(requestBuilder)
+                    .andExpectAll(
+                            status().isUnauthorized(),
+                            jsonPath("$.message").value(AUTH_EXPIRED_TOKEN.getMessage())
+                    );
+        }
+    }
+
+    private MockHttpServletRequestBuilder buildReissueRequest() {
+        return MockMvcRequestBuilders
+                .post(BASE_URL)
+                .header(AUTHORIZATION, BEARER + REFRESH_TOKEN);
+    }
+
+    private void setSuccessService() {
+        given(provider.isValidToken(REFRESH_TOKEN)).willReturn(true);
+        given(provider.getId(REFRESH_TOKEN)).willReturn(1L);
     }
 }
