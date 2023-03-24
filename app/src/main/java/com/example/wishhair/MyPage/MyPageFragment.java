059694d61c1cf1ecc3f634616ad3bca1d5c57a01
treesnap <treenams4878@gmail.com>
2023-03-24 10:07:50 +0900

    branch update

--- app/src/main/java/com/example/wishhair/MyPage/MyPageFragment.java
+++ app/src/main/java/com/example/wishhair/MyPage/MyPageFragment.java
@@ -1,14 +1,12 @@
 package com.example.wishhair.MyPage;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,37 +16,55 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.wishhair.MainActivity;
 import com.example.wishhair.MyPage.adapters.MyPageRecyclerViewAdapter;
 import com.example.wishhair.MyPage.items.HeartlistItem;
 import com.example.wishhair.R;
-import com.example.wishhair.sign.UrlConst;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MyPageFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class MyPageFragment extends Fragment {
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     MainActivity mainActivity;
     RecyclerView HeartlistRecyclerView;
     MyPageRecyclerViewAdapter adapter;
     RecyclerDecoration recyclerDecoration;
+//    private final String URL = "https://afa5-182-222-186-10.jp.ngrok.io";
 
-    private SharedPreferences loginSP;
-    final static private String url = UrlConst.URL + "/api/logout";
-    String nickname = null;
 
     public MyPageFragment() {
         // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment4.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MyPageFragment newInstance(String param1, String param2) {
+        MyPageFragment fragment = new MyPageFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
@@ -60,6 +76,10 @@ public class MyPageFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
@@ -121,25 +141,6 @@ public class MyPageFragment extends Fragment {
             }
         });
         toolbar.inflateMenu(R.menu.메뉴.xml);   버튼 추가 시 사용할 것 */
-
-//        LOGOUT
-        Button btn_logout  = view.findViewById(R.id.mypage_button_logout);
-
-        loginSP = getActivity().getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
-        String accessToken = loginSP.getString("accessToken", "fail acc");
-
-        Log.d("acc", loginSP.getString("accessToken", "fail acc"));
-        Log.d("ref", loginSP.getString("refreshToken", "fail ref"));
-
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logout(accessToken);
-            }
-        });
-
-        myPageRequest();
-
     }
 
     @Override
@@ -149,68 +150,5 @@ public class MyPageFragment extends Fragment {
         return view;
     }
 
-    private void logout(String accessToken) {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url , null, new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                logout_delete_token(loginSP);
-
-//                TODO : 현재 액티비티(프래그먼트)를 끝내고 LoginActivity로 이동
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                logout_delete_token(loginSP);
-
-//                TODO : 현재 액티비티(프래그먼트)를 끝내고 LoginActivity로 이동
-            }
-
-        }) {
-
-            @Override
-            public Map<String, String> getHeaders() {
-                Map<String, String>  params = new HashMap();
-                params.put("Authorization", "bearer" + accessToken);
-
-                return params;
-            }
-        };
-
-        RequestQueue queue = Volley.newRequestQueue(requireContext());
-        queue.add(jsonObjectRequest);
-    }
-
-    private void logout_delete_token (SharedPreferences sharedPreferences) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.remove("accessToken");
-        editor.remove("refreshToken");
-        editor.apply();
-    }
-
-    public void myPageRequest() {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url , null, new Response.Listener<JSONObject>() {
-
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    nickname = response.getString("nickname");
-                    Log.i("받아온 거", nickname);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-            }
-        });
-
-        RequestQueue queue = Volley.newRequestQueue(getContext());
-        queue.add(jsonObjectRequest);
-    }
 
 }
