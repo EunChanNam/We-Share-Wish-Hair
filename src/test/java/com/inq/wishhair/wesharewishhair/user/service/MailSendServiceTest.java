0df0a6521327c800a7cb94e6a872cdec567e71c7
EunChanNam <namhm23@gmail.com>
2023-03-24 11:45:00 +0900

    [Test] MailSendService - 만들어진 메일 정보를 통해 메일을 전송한다

--- src/test/java/com/inq/wishhair/wesharewishhair/user/service/MailSendServiceTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/user/service/MailSendServiceTest.java
@@ -1,12 +1,25 @@
 package com.inq.wishhair.wesharewishhair.user.service;
 
 import com.inq.wishhair.wesharewishhair.global.base.ServiceTest;
+import com.inq.wishhair.wesharewishhair.user.service.dto.MailDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.inq.wishhair.wesharewishhair.fixture.MailDtoFixture.A;
 
 public class MailSendServiceTest extends ServiceTest {
 
     @Autowired
     private MailSendService mailSendService;
 
+    @Test
+    @DisplayName("만들어진 메일 정보를 통해 메일을 전송한다")
+    void test1() {
+        //given
+        MailDto mailDto = A.toMailDto();
 
+        //when, then -> 성공하면 성공적으로 메일이 발송된다.
+        mailSendService.sendAuthorizationMail(mailDto);
+    }
 }
