1370be7e9c0e089b21fc0f177dcbfc5aac9537f7
EunChanNam <namhm23@gmail.com>
2023-03-25 11:12:02 +0900

    [Edit] 페이징 정렬조건 상수 NEW, OLD -> DATE 로 통합해서 수정

--- src/main/java/com/inq/wishhair/wesharewishhair/review/common/ReviewSortCondition.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/review/common/ReviewSortCondition.java
@@ -1,7 +1,6 @@
 package com.inq.wishhair.wesharewishhair.review.common;
 
 public interface ReviewSortCondition {
-    String LIKES = "likes,desc";
-    String NEW = "createDate,desc";
-    String OLD = "createDate";
+    String LIKES = "likes";
+    String DATE = "createdDate";
 }
