ed5a50664c31b3a51e686d6881d57d96071a6ae1
EunChanNam <namhm23@gmail.com>
2023-03-25 15:10:34 +0900

    [Feature] PointFindService - findPointHistories 구현

--- src/main/java/com/inq/wishhair/wesharewishhair/user/domain/point/PointFindRepository.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/user/domain/point/PointFindRepository.java
@@ -1,18 +1,18 @@
 package com.inq.wishhair.wesharewishhair.user.domain.point;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.geo.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface PointFindRepository extends JpaRepository<Point, Long> {
 
     @Query("select p from PointHistory p " +
             "where p.user.id = :userId " +
             "order by p.createdDate desc")
-    List<PointHistory> findByUserIdOrderByNew(@Param("userId") Long userId,
-                                              Pageable pageable);
+    Slice<PointHistory> findByUserIdOrderByNew(@Param("userId") Long userId,
+                                               Pageable pageable);
 }
--- src/main/java/com/inq/wishhair/wesharewishhair/user/service/PointFindService.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/user/service/PointFindService.java
@@ -1,7 +1,11 @@
 package com.inq.wishhair.wesharewishhair.user.service;
 
 import com.inq.wishhair.wesharewishhair.user.domain.point.PointFindRepository;
+import com.inq.wishhair.wesharewishhair.user.domain.point.PointHistory;
+import com.inq.wishhair.wesharewishhair.user.service.dto.response.PointResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class PointFindService {
 
     private final PointFindRepository pointFindRepository;
+
+    public Slice<PointResponse> findPointHistories(Long userId, Pageable pageable) {
+
+        return transferContentToResponse(pointFindRepository.findByUserIdOrderByNew(userId, pageable));
+    }
+
+    private Slice<PointResponse> transferContentToResponse(Slice<PointHistory> slice) {
+        return slice.map(PointResponse::new);
+    }
 }
