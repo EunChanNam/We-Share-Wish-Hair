31dca5b37288c653fcb71743579d0c9a3a9cb6f8
EunChanNam <namhm23@gmail.com>
2023-03-25 15:30:24 +0900

    [Fix] PointFindReponsitory 제네릭 타입 수정

--- src/main/java/com/inq/wishhair/wesharewishhair/user/domain/point/PointRepository.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/user/domain/point/PointRepository.java
@@ -1,11 +1,6 @@
 package com.inq.wishhair.wesharewishhair.user.domain.point;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface PointRepository extends JpaRepository<PointHistory, Long> {
 }
