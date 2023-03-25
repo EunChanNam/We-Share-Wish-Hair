976082015ebd1286b7d36201fea4b6ae04881ffe
EunChanNam <namhm23@gmail.com>
2023-03-25 20:47:29 +0900

    [Edit] PagedWishListResponse 에 적용

--- src/main/java/com/inq/wishhair/wesharewishhair/wishlist/service/dto/response/WishListResponse.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/wishlist/service/dto/response/WishListResponse.java
@@ -19,7 +19,6 @@ public class WishListResponse {
     public WishListResponse(HairStyle hairStyle) {
         this.hairStyleId = hairStyle.getId();
         this.hairStyleName = hairStyle.getName();
-        // fetch join 을 했기 때문에 초기화가 됐다는 것은 사진이 있다는 말이다.
         if (Persistence.getPersistenceUtil().isLoaded(hairStyle.getPhotos())) {
             this.photoResponse = new PhotoResponse(hairStyle.getPhotos().get(0));
         }
