bd0440baefbdbbcf8226e6b931df775858a49152
EunChanNam <namhm23@gmail.com>
2023-03-25 12:44:15 +0900

    [Feature] PointHistories - insertPointHistory 정의

--- src/main/java/com/inq/wishhair/wesharewishhair/user/domain/point/PointHistories.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/user/domain/point/PointHistories.java
@@ -1,5 +1,6 @@
 package com.inq.wishhair.wesharewishhair.user.domain.point;
 
+import com.inq.wishhair.wesharewishhair.user.domain.User;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.OneToMany;
@@ -19,6 +20,18 @@ public class PointHistories {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
     private List<PointHistory> pointHistories = new ArrayList<>();
+
+    public void insertPointHistory(User user, PointType pointType, int dealAmount) {
+        if (pointType.isCharge()) {
+            PointHistory chargePointHistory =
+                    PointHistory.createPointHistory(user, pointType, dealAmount, availablePoint + dealAmount);
+            pointHistories.add(chargePointHistory);
+        } else if (pointType.isUSE()) {
+            PointHistory usePointHistory =
+                    PointHistory.createPointHistory(user, pointType, dealAmount, availablePoint - dealAmount);
+            pointHistories.add(usePointHistory);
+        }
+    }
 
 
 }
