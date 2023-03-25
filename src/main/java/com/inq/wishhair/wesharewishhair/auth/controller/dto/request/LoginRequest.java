c8151e3d7f1e47d07e892a0b0d4cf909f80ba613
EunChanNam <namhm23@gmail.com>
2023-03-25 10:51:50 +0900

    [Edit] 모든 Request 에 @NoArgsConstructor 추가

--- src/main/java/com/inq/wishhair/wesharewishhair/auth/controller/dto/request/LoginRequest.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/auth/controller/dto/request/LoginRequest.java
@@ -1,9 +1,12 @@
 package com.inq.wishhair.wesharewishhair.auth.controller.dto.request;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class LoginRequest {
 
--- src/main/java/com/inq/wishhair/wesharewishhair/review/controller/dto/request/ReviewRequest.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/review/controller/dto/request/ReviewRequest.java
@@ -2,14 +2,17 @@ package com.inq.wishhair.wesharewishhair.review.controller.dto.request;
 
 import com.inq.wishhair.wesharewishhair.review.enums.Score;
 import com.inq.wishhair.wesharewishhair.review.service.dto.ReviewCreateDto;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ReviewRequest {
 
