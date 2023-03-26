248b38fed5306b680bba90ad7c222675fc860c12
EunChanNam <namhm23@gmail.com>
2023-03-26 20:56:34 +0900

    [Edit] 테스트 코드 맞춰서 수정

--- src/main/java/com/inq/wishhair/wesharewishhair/user/controller/dto/response/SessionIdResponse.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/user/controller/dto/response/SessionIdResponse.java
@@ -1,10 +1,5 @@
 package com.inq.wishhair.wesharewishhair.user.controller.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public record SessionIdResponse(String sessionId) {
 
 }
