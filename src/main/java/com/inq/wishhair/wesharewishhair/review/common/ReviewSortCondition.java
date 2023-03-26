ad53adef848b22130a3c1a965c542e9230460dde
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-03-26 21:53:05 +0900

    [BE] likes 정렬 버그 픽스 #101

--- src/main/java/com/inq/wishhair/wesharewishhair/review/common/ReviewSortCondition.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/review/common/ReviewSortCondition.java
@@ -1,6 +1,6 @@
 package com.inq.wishhair.wesharewishhair.review.common;
 
 public interface ReviewSortCondition {
-    String LIKES = "likes";
+    String LIKES = "likeReviews.likes";
     String DATE = "createdDate";
 }
