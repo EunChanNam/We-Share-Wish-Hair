d0fb81857152bec5b9a350a387a6099462a88047
EunChanNam <namhm23@gmail.com>
2023-03-29 13:20:45 +0900

    [Dev] 모든 Controller ControllerAdvice 에 추가

--- src/main/java/com/inq/wishhair/wesharewishhair/global/exception/ApiExceptionHandler.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/global/exception/ApiExceptionHandler.java
@@ -1,13 +1,14 @@
 package com.inq.wishhair.wesharewishhair.global.exception;
 
 import com.inq.wishhair.wesharewishhair.auth.controller.TokenReissueController;
-import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
-import com.inq.wishhair.wesharewishhair.global.exception.ErrorResponse;
-import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
 import com.inq.wishhair.wesharewishhair.auth.controller.AuthController;
 import com.inq.wishhair.wesharewishhair.hairstyle.controller.HairStyleController;
+import com.inq.wishhair.wesharewishhair.review.controller.LikeReviewController;
 import com.inq.wishhair.wesharewishhair.review.controller.ReviewController;
+import com.inq.wishhair.wesharewishhair.review.controller.ReviewFindController;
 import com.inq.wishhair.wesharewishhair.user.controller.MailController;
+import com.inq.wishhair.wesharewishhair.user.controller.MyPageController;
+import com.inq.wishhair.wesharewishhair.user.controller.PointFindController;
 import com.inq.wishhair.wesharewishhair.user.controller.UserController;
 import com.inq.wishhair.wesharewishhair.wishlist.controller.WishListController;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(assignableTypes = {
         UserController.class, AuthController.class, HairStyleController.class,
         ReviewController.class, WishListController.class, AuthController.class,
-        TokenReissueController.class, MailController.class
+        TokenReissueController.class, MailController.class, MyPageController.class,
+        LikeReviewController.class, ReviewFindController.class, PointFindController.class
 })
 public class ApiExceptionHandler {
 
