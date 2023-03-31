2297255bce656385243365ffab92e7667d176595
EunChanNam <namhm23@gmail.com>
2023-03-31 14:32:54 +0900

    [Dev] PointController - usePoint API 구현

--- src/main/java/com/inq/wishhair/wesharewishhair/user/controller/PointController.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/user/controller/PointController.java
@@ -1,7 +1,13 @@
 package com.inq.wishhair.wesharewishhair.user.controller;
 
+import com.inq.wishhair.wesharewishhair.auth.config.resolver.ExtractPayload;
+import com.inq.wishhair.wesharewishhair.global.dto.response.Success;
+import com.inq.wishhair.wesharewishhair.user.controller.dto.request.PointUseRequest;
 import com.inq.wishhair.wesharewishhair.user.service.PointService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class PointController {
 
     private final PointService pointService;
+
+    @PostMapping("/use")
+    public ResponseEntity<Success> usePoint(@RequestBody PointUseRequest request,
+                                            @ExtractPayload Long userId) {
+
+        pointService.usePoint(request, userId);
+
+        return ResponseEntity.ok(new Success());
+    }
 }
