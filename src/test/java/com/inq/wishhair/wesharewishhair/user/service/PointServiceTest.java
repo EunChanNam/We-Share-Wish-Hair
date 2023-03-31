617acb9b87a2fd8d1d8de62f8f2be3e5b94a4d2e
EunChanNam <namhm23@gmail.com>
2023-03-31 16:17:25 +0900

    [Test] PointServiceTest - 올바르지 않은 dealAmount 로 충전에 실패한다

---
+++ src/test/java/com/inq/wishhair/wesharewishhair/user/service/PointServiceTest.java
@@ -0,0 +1,48 @@
+package com.inq.wishhair.wesharewishhair.user.service;
+
+import com.inq.wishhair.wesharewishhair.fixture.UserFixture;
+import com.inq.wishhair.wesharewishhair.global.base.ServiceTest;
+import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
+import com.inq.wishhair.wesharewishhair.user.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("PointServiceTest - SpringBootTest")
+public class PointServiceTest extends ServiceTest {
+
+    @Autowired
+    private PointService pointService;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        //given
+        user = UserFixture.A.toEntity();
+        userRepository.save(user);
+    }
+
+    @Nested
+    @DisplayName("포인트 충전 서비스")
+    class chargePoint {
+        @Test
+        @DisplayName("올바르지 않은 dealAmount 로 충전에 실패한다")
+        void test1() {
+            //given
+            ErrorCode expectedError = ErrorCode.POINT_INVALID_POINT_RANGE;
+
+            //when, then
+            assertThatThrownBy(() -> pointService.chargePoint(-1000, user.getId()))
+                    .isInstanceOf(WishHairException.class)
+                    .hasMessageContaining(expectedError.getMessage());
+        }
+    }
+
+}
