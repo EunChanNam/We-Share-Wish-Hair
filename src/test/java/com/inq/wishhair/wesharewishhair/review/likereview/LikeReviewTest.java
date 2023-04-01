53e0279617866ce4328cbeeae64200a7f03bfeed
EunChanNam <namhm23@gmail.com>
2023-04-01 22:58:01 +0900

    [Test] LikeReviewTest - 현재 좋아요의 유저와 입력된 유저가 같은지 확인한다

--- src/test/java/com/inq/wishhair/wesharewishhair/review/likereview/LikeReviewTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/review/likereview/LikeReviewTest.java
@@ -7,11 +7,15 @@ import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyle;
 import com.inq.wishhair.wesharewishhair.review.domain.Review;
 import com.inq.wishhair.wesharewishhair.review.domain.likereview.LikeReview;
 import com.inq.wishhair.wesharewishhair.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.BDDMockito.given;
 
 @DisplayName("Review-LikeReview 도메인 테스트")
 public class LikeReviewTest {
@@ -32,5 +36,43 @@ public class LikeReviewTest {
                 () -> assertThat(likeReview.getReview()).isEqualTo(review),
                 () -> assertThat(likeReview.getUser()).isEqualTo(user)
         );
+    }
+
+    @Nested
+    @DisplayName("현재 좋아요의 유저와 입력된 유저가 같은지 확인한다")
+    class isSameUser {
+
+        private LikeReview likeReview;
+
+        @BeforeEach
+        void setUp() {
+            //given
+            User user = Mockito.mock(User.class);
+            HairStyle hairStyle = HairStyleFixture.A.toEntity();
+            Review review = ReviewFixture.A.toEntity(user, hairStyle);
+            likeReview = LikeReview.createLikeReview(user, review);
+
+            given(user.getId()).willReturn(1L);
+        }
+
+        @Test
+        @DisplayName("같은 유저를 입력받아 true 를 반환한다")
+        void test2() {
+            //when
+            boolean result = likeReview.isSameUser(1L);
+
+            //then
+            assertThat(result).isTrue();
+        }
+
+        @Test
+        @DisplayName("다른 유저를 입력받아 false 를 반환한다")
+        void test3() {
+            //when
+            boolean result = likeReview.isSameUser(2L);
+
+            //then
+            assertThat(result).isFalse();
+        }
     }
 }
