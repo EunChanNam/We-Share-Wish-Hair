a54ecdf9a67bd771004f556f871ddd060cde36d8
EunChanNam <namhm23@gmail.com>
2023-04-02 14:16:27 +0900

    [Dev] 리뷰 삭제 API 구현

--- src/main/java/com/inq/wishhair/wesharewishhair/review/controller/ReviewController.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/review/controller/ReviewController.java
@@ -2,8 +2,9 @@ package com.inq.wishhair.wesharewishhair.review.controller;
 
 import com.inq.wishhair.wesharewishhair.auth.config.resolver.ExtractPayload;
 import com.inq.wishhair.wesharewishhair.global.dto.response.Success;
+import com.inq.wishhair.wesharewishhair.review.controller.dto.request.ReviewDeleteRequest;
 import com.inq.wishhair.wesharewishhair.review.service.ReviewService;
-import com.inq.wishhair.wesharewishhair.review.controller.dto.request.ReviewRequest;
+import com.inq.wishhair.wesharewishhair.review.controller.dto.request.ReviewCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +20,21 @@ public class ReviewController {
 
     @PostMapping
     public ResponseEntity<Success> createReview(
-            @ModelAttribute ReviewRequest reviewRequest,
+            @ModelAttribute ReviewCreateRequest reviewCreateRequest,
             @ExtractPayload Long userId) {
 
-        Long reviewId = reviewService.createReview(reviewRequest.toReviewCreateDto(userId));
+        Long reviewId = reviewService.createReview(reviewCreateRequest.toReviewCreateDto(userId));
         return ResponseEntity
                 .created(URI.create("/api/review/" + reviewId))
                 .body(new Success());
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Success> deleteReview(@ExtractPayload Long userId,
+                                                @RequestBody ReviewDeleteRequest request) {
+
+        reviewService.deleteReview(request.getReviewId(), userId);
+
+        return ResponseEntity.ok(new Success());
     }
 }
