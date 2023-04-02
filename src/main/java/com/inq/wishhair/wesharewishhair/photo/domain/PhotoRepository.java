eef59ca599403cbc4919772619fdce69ab845e0a
EunChanNam <namhm23@gmail.com>
2023-04-02 14:22:05 +0900

    [Dev] PhotoRepository 구현

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/domain/PhotoRepository.java
@@ -0,0 +1,6 @@
+package com.inq.wishhair.wesharewishhair.photo.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+}
