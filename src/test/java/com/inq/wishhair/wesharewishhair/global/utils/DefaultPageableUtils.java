23019f9916e11f57cf99797bcbd16c9fa74b603c
EunChanNam <namhm23@gmail.com>
2023-04-03 16:16:46 +0900

    [Test] ReviewFindControllerTest - 전체 리뷰 조회 API 테스트

---
+++ src/test/java/com/inq/wishhair/wesharewishhair/global/utils/DefaultPageableUtils.java
@@ -0,0 +1,13 @@
+package com.inq.wishhair.wesharewishhair.global.utils;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import static com.inq.wishhair.wesharewishhair.review.common.ReviewSortCondition.LIKES;
+
+public abstract class DefaultPageableUtils {
+    public static Pageable getLikeDescPageable() {
+        return PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, LIKES));
+    }
+}
