0525898ff92f05dd22c185045054b2713ae34688
EunChanNam <namhm23@gmail.com>
2023-04-05 20:28:04 +0900

    중간 저장

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/FileDir.java
@@ -0,0 +1,5 @@
+package com.inq.wishhair.wesharewishhair.photo.dto.response;
+
+interface FileDir {
+    String VALUE = "/Users/eunchan/Desktop/Projects/we-share-wish-hair/src/main/resources/static/images/review/";
+}
--- src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
@@ -4,13 +4,30 @@ import com.inq.wishhair.wesharewishhair.photo.entity.Photo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Base64;
+
 @Getter
 @AllArgsConstructor
 public class PhotoResponse {
 
-    private final String storeFilename;
+    private final String encodedFullPath;
 
     public PhotoResponse(Photo photo) {
-        this.storeFilename = photo.getStoreFilename();
+        String fullPath = FileDir.VALUE + photo.getStoreFilename();
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fullPath);
+
+            file
+
+            Base64.getEncoder().encode()
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
