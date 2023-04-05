60e85213a601f1c2300ede2ad4311175e8510b84
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-04-06 01:53:13 +0900

    Revert "[Edit] Photostore 리펙토링 (#157)" (#159)
    This reverts commit a6ac5fc6b71ba436208f5f49cfab7e220001a756.

--- src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/FileDir.java
+++
@@ -1,5 +0,0 @@
-package com.inq.wishhair.wesharewishhair.photo.dto.response;
-
-interface FileDir {
-    String VALUE = "/Users/eunchan/Desktop/Projects/we-share-wish-hair/src/main/resources/static/images/review/";
-}
