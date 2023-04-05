0573a60f669e62b944994d08e616d62b2cdd3cfe
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-04-06 01:52:26 +0900

    Revert "[Edit] Photostore 리펙토링 (#157)"
    This reverts commit a6ac5fc6b71ba436208f5f49cfab7e220001a756.

--- src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
@@ -4,30 +4,13 @@ import com.inq.wishhair.wesharewishhair.photo.domain.Photo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Base64;
-
 @Getter
 @AllArgsConstructor
 public class PhotoResponse {
 
-    private final String encodedImage;
+    private final String storeFilename;
 
     public PhotoResponse(Photo photo) {
-        String fullPath = FileDir.VALUE + photo.getStoreFilename();
-        File file = new File(fullPath);
-
-        byte[] imageBytes = new byte[(int) file.length()];
-        try {
-            FileInputStream fileInputStream = new FileInputStream(file);
-            fileInputStream.read(imageBytes);
-            fileInputStream.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        encodedImage = Base64.getEncoder().encodeToString(imageBytes);
+        this.storeFilename = photo.getStoreFilename();
     }
 }
