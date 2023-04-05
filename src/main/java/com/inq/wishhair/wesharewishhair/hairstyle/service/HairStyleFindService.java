20d656282cfbb0798c1fe85153b0464c36e259ca
EunChanNam <namhm23@gmail.com>
2023-04-05 18:48:01 +0900

    [Refactor] ReviewService - UserFindService, hairStyleFindService 적용

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/hairstyle/service/HairStyleFindService.java
@@ -0,0 +1,20 @@
+package com.inq.wishhair.wesharewishhair.hairstyle.service;
+
+import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyle;
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HairStyleFindService {
+
+    private final HairStyleRepository hairStyleRepository;
+
+    public HairStyle findById(Long id) {
+        return hairStyleRepository.findById(id)
+                .orElseThrow(() -> new WishHairException(ErrorCode.NOT_EXIST_KEY));
+    }
+}
