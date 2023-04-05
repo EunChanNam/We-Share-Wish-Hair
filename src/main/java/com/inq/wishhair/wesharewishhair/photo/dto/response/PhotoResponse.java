a6ac5fc6b71ba436208f5f49cfab7e220001a756
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-04-06 01:28:42 +0900

    [Edit] Photostore 리펙토링 (#157)
    * [Edit] PhotoResponse - 응답 이미지 바이트 데이터로 변경

--- src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
@@ -4,13 +4,30 @@ import com.inq.wishhair.wesharewishhair.photo.domain.Photo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Base64;
+
 @Getter
 @AllArgsConstructor
 public class PhotoResponse {
 
-    private final String storeFilename;
+    private final String encodedImage;
 
     public PhotoResponse(Photo photo) {
-        this.storeFilename = photo.getStoreFilename();
+        String fullPath = FileDir.VALUE + photo.getStoreFilename();
+        File file = new File(fullPath);
+
+        byte[] imageBytes = new byte[(int) file.length()];
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            fileInputStream.read(imageBytes);
+            fileInputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        encodedImage = Base64.getEncoder().encodeToString(imageBytes);
     }
 }
