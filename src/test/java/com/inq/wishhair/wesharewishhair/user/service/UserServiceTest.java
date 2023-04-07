c0c5b442e5168b7c2eee6f261d1ad07ba4d5768b
EunChanNam <namhm23@gmail.com>
2023-04-07 16:08:02 +0900

    [Test] UserServiceTest - 회원 정보 수정 서비스

--- src/test/java/com/inq/wishhair/wesharewishhair/user/service/UserServiceTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/user/service/UserServiceTest.java
@@ -1,12 +1,12 @@
 package com.inq.wishhair.wesharewishhair.user.service;
 
+import com.inq.wishhair.wesharewishhair.fixture.UserFixture;
 import com.inq.wishhair.wesharewishhair.global.base.ServiceTest;
-import com.inq.wishhair.wesharewishhair.user.controller.utils.UserCreateRequestUtils;
-import com.inq.wishhair.wesharewishhair.user.domain.point.PointHistory;
+import com.inq.wishhair.wesharewishhair.user.controller.dto.request.UserUpdateRequest;
+import com.inq.wishhair.wesharewishhair.user.controller.utils.UserUpdateRequestUtils;
 import com.inq.wishhair.wesharewishhair.user.domain.User;
 import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
 import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
-import com.inq.wishhair.wesharewishhair.fixture.UserFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -57,6 +57,22 @@ class UserServiceTest extends ServiceTest {
     @Nested
     @DisplayName("회원 탈퇴 서비스")
     class deleteUser {
+        @Test
+        @DisplayName("회원 탈퇴에 성공한다")
+        void success() {
+            //when
+            User user = userRepository.save(B.toEntity());
+            userService.deleteUser(user.getId());
+
+            //then
+            List<User> result = userRepository.findAll();
+            assertThat(result).isEmpty();
+        }
+    }
+
+    @Nested
+    @DisplayName("회원 정보 수정 서비스")
+    class updateUser {
 
         private User user;
 
@@ -67,14 +83,44 @@ class UserServiceTest extends ServiceTest {
         }
 
         @Test
-        @DisplayName("회원 탈퇴에 성공한다")
+        @DisplayName("회원의 닉네임과 성별을 수정에 성공한다")
         void success() {
+            //given
+            UserUpdateRequest request = UserUpdateRequestUtils.request(A);
+
             //when
-            userService.deleteUser(user.getId());
+            userService.updateUser(user.getId(), request);
 
             //then
-            List<User> result = userRepository.findAll();
-            assertThat(result).isEmpty();
+            assertAll(
+                    () -> assertThat(user.getNicknameValue()).isEqualTo(request.getNickname()),
+                    () -> assertThat(user.getSex()).isEqualTo(request.getSex())
+            );
+
+        }
+
+        @Test
+        @DisplayName("틀린 형식의 닉네임으로 수정에 실패힌다")
+        void failByNickname() {
+            //given
+            UserUpdateRequest request = UserUpdateRequestUtils.wrongNicknameRequest(A);
+
+            //when, then
+            assertThatThrownBy(() -> userService.updateUser(user.getId(), request))
+                    .isInstanceOf(WishHairException.class)
+                    .hasMessageContaining(ErrorCode.USER_INVALID_NICKNAME.getMessage());
+        }
+
+        @Test
+        @DisplayName("중복된 닉네임으로 수정에 실패한다")
+        void failByDuplicatedNickname() {
+            //given
+            UserUpdateRequest request = UserUpdateRequestUtils.request(B);
+
+            //when, then
+            assertThatThrownBy(() -> userService.updateUser(user.getId(), request))
+                    .isInstanceOf(WishHairException.class)
+                    .hasMessageContaining(ErrorCode.USER_DUPLICATED_NICKNAME.getMessage());
         }
     }
 }
