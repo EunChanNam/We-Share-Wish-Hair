67b9e9c66eb86d349a276c71a33fb5169fcdbf02
EunChanNam <namhm23@gmail.com>
2023-04-07 14:17:07 +0900

    UserControllerTest - 올바르지 않은 이메일 형식으로 400 예외를 던진다

--- src/test/java/com/inq/wishhair/wesharewishhair/user/controller/UserControllerTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/user/controller/UserControllerTest.java
@@ -1,15 +1,20 @@
 package com.inq.wishhair.wesharewishhair.user.controller;
 
 import com.inq.wishhair.wesharewishhair.global.base.ControllerTest;
+import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
 import com.inq.wishhair.wesharewishhair.user.controller.utils.UserCreateRequestUtils;
 import com.inq.wishhair.wesharewishhair.user.controller.dto.request.UserCreateRequest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.inq.wishhair.wesharewishhair.user.controller.utils.UserCreateRequestUtils.*;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("UserControllerTest - Mock")
@@ -17,23 +22,52 @@ public class UserControllerTest extends ControllerTest {
 
     private static final String JOIN_URL = "/api/user";
 
-    @Test
-    @DisplayName("성공적인 회원가입 테스트")
-    void successCreateUserTest() throws Exception {
+    @Nested
+    @DisplayName("회원가입 API 테스트")
+    class createUser {
+        @Test
+        @DisplayName("성공적인 회원가입 테스트")
+        void success() throws Exception {
+            //given
+            UserCreateRequest request = successRequest();
+            given(userService.createUser(request.toEntity())).willReturn(1L);
 
-        UserCreateRequest request = UserCreateRequestUtils.createRequest();
-        given(userService.createUser(request.toEntity())).willReturn(1L);
+            //when
+            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .post(JOIN_URL)
+                    .content(toJson(request))
+                    .contentType(APPLICATION_JSON);
 
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
-                .post(JOIN_URL)
-                .content(toJson(request))
-                .contentType(MediaType.APPLICATION_JSON);
+            //then
+            mockMvc.perform(requestBuilder)
+                    .andExpectAll(
+                            status().isCreated(),
+                            jsonPath("$").exists(),
+                            jsonPath("$.success").value(true)
+                    );
+        }
 
-        mockMvc.perform(requestBuilder)
-                .andExpectAll(
-                        status().isCreated(),
-                        jsonPath("$").exists(),
-                        jsonPath("$.success").value(true)
-                );
+        @Test
+        @DisplayName("올바르지 않은 이메일 형식으로 400 예외를 던진다")
+        void failByEmail() throws Exception {
+            //given
+            UserCreateRequest request = WrongEmailRequest();
+
+            ErrorCode expectedError = ErrorCode.USER_INVALID_EMAIL;
+
+            //when
+            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .post(JOIN_URL)
+                    .content(toJson(request))
+                    .contentType(APPLICATION_JSON);
+
+            //then
+            mockMvc.perform(requestBuilder)
+                    .andExpectAll(
+                            status().isBadRequest(),
+                            jsonPath("$").exists(),
+                            jsonPath("$.message").value(expectedError.getMessage())
+                    );
+        }
     }
 }
--- src/test/java/com/inq/wishhair/wesharewishhair/user/controller/utils/UserCreateRequestUtils.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/user/controller/utils/UserCreateRequestUtils.java
@@ -6,12 +6,42 @@ import com.inq.wishhair.wesharewishhair.user.controller.dto.request.UserCreateRe
 public abstract class UserCreateRequestUtils {
     private static final UserFixture a = UserFixture.A;
 
-    public static UserCreateRequest createRequest() {
+    public static UserCreateRequest successRequest() {
         return new UserCreateRequest(
                 a.getEmail(),
                 a.getPassword(),
                 a.getName(),
                 a.getNickname(),
+                a.getSex()
+        );
+    }
+
+    public static UserCreateRequest WrongEmailRequest() {
+        return new UserCreateRequest(
+                "wrongEmail121",
+                a.getPassword(),
+                a.getName(),
+                a.getNickname(),
+                a.getSex()
+        );
+    }
+
+    public static UserCreateRequest WrongPasswordRequest() {
+        return new UserCreateRequest(
+                a.getEmail(),
+                "12341234",
+                a.getName(),
+                a.getNickname(),
+                a.getSex()
+        );
+    }
+
+    public static UserCreateRequest WrongNicknameRequest() {
+        return new UserCreateRequest(
+                a.getEmail(),
+                a.getPassword(),
+                a.getName(),
+                "닉",
                 a.getSex()
         );
     }
