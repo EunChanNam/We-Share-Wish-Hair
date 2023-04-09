b3d76754fde04e8d84d3c850185fe35455672ad4
EunChanNam <namhm23@gmail.com>
2023-04-09 15:33:24 +0900

    [Test] LikeReviewControllerTest - 토큰을 포함하지 않을경우 추가 테스트

--- src/test/java/com/inq/wishhair/wesharewishhair/review/controller/LikeReviewControllerTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/review/controller/LikeReviewControllerTest.java
@@ -1,7 +1,9 @@
 package com.inq.wishhair.wesharewishhair.review.controller;
 
 import com.inq.wishhair.wesharewishhair.global.base.ControllerTest;
+import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,20 +17,38 @@ public class LikeReviewControllerTest extends ControllerTest {
 
     private static final String BASE_URL = "/api/review/like";
 
-    @Test
+    @Nested
     @DisplayName("좋아요 API 테스트")
-    void likeReview() throws Exception {
-        //when
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
-                .post(BASE_URL + "/1")
-                .header(AUTHORIZATION, BEARER + ACCESS_TOKEN);
+    class likeReview {
+        @Test
+        @DisplayName("헤더에 Access 토큰을 포함하지 않아서 예외를 던진다")
+        void failByNoAccessToken() throws Exception {
+            //given
+            ErrorCode expectedError = ErrorCode.AUTH_REQUIRED_LOGIN;
 
-        //then
-        mockMvc.perform(requestBuilder)
-                .andExpectAll(
-                        status().isOk(),
-                        jsonPath("$").exists(),
-                        jsonPath("$.success").value(true)
-                );
+            //when
+            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .post(BASE_URL + "/1");
+
+            //then
+            assertException(expectedError, requestBuilder, status().isUnauthorized());
+        }
+
+        @Test
+        @DisplayName("좋아요를 실행한다")
+        void success() throws Exception {
+            //when
+            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .post(BASE_URL + "/1")
+                    .header(AUTHORIZATION, BEARER + ACCESS_TOKEN);
+
+            //then
+            mockMvc.perform(requestBuilder)
+                    .andExpectAll(
+                            status().isOk(),
+                            jsonPath("$").exists(),
+                            jsonPath("$.success").value(true)
+                    );
+        }
     }
 }
