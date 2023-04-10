2ec854d30f1e0f198f0924ed02f123a1ea46d45b
EunChanNam <namhm23@gmail.com>
2023-04-10 15:21:43 +0900

    [Setting] restdocs 템플릿 및 부가 설정

--- src/test/java/com/inq/wishhair/wesharewishhair/global/base/ControllerTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/global/base/ControllerTest.java
@@ -19,22 +19,40 @@ import com.inq.wishhair.wesharewishhair.user.controller.*;
 import com.inq.wishhair.wesharewishhair.user.service.*;
 import com.inq.wishhair.wesharewishhair.hairstyle.controller.HairStyleController;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static com.inq.wishhair.wesharewishhair.global.utils.TokenUtils.ACCESS_TOKEN;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(value =
         {UserController.class, HairStyleController.class, AuthController.class, TokenReissueController.class,
         HairStyleController.class, MailController.class, ReviewController.class, ReviewSearchController.class,
         LikeReviewController.class, MyPageController.class, PointController.class, PointSearchController.class})
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 public abstract class ControllerTest {
+
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
 
     @Autowired
     protected MockMvc mockMvc;
@@ -84,10 +102,25 @@ public abstract class ControllerTest {
     }
 
     @BeforeEach
-    void setUp() {
+    void setUpLogin() {
         //given
         given(provider.isValidToken(ACCESS_TOKEN)).willReturn(true);
         given(provider.getId(ACCESS_TOKEN)).willReturn(1L);
+    }
+
+    @BeforeEach
+    void setUp(final WebApplicationContext context,
+               final RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(MockMvcRestDocumentation.documentationConfiguration(provider))  // rest docs 설정 주입
+                .alwaysDo(print()) // andDo(print()) 코드 포함
+                .alwaysDo(restDocs)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true)) // 한글 깨짐 방지
+                .build();
+    }
+
+    protected Attributes.Attribute constraint(String value) {
+        return new Attributes.Attribute("constraints", value);
     }
 
     protected void assertException(ErrorCode expectedError,
