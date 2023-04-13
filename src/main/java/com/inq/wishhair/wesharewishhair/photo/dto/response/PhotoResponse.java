2cf555b5ac063d1645b0878b4e6f397c6dfdd8d0
EunChanNam <namhm23@gmail.com>
2023-04-13 18:49:30 +0900

    [Feat] PhotoResponse 응답 byte 데이터로 변경

--- src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
@@ -4,13 +4,32 @@ import com.inq.wishhair.wesharewishhair.photo.domain.Photo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Base64;
+
 @Getter
 @AllArgsConstructor
 public class PhotoResponse {
 
-    private final String storeFilename;
+    private final String encodedImage;
 
     public PhotoResponse(Photo photo) {
-        this.storeFilename = photo.getStoreFilename();
+        String filePath = FilePath.VALUE;
+        File image = new File(filePath + photo.getStoreFilename());
+
+        byte[] byteImage = new byte[(int) image.length()];
+
+        try {
+            FileInputStream inputStream = new FileInputStream(image);
+            inputStream.read(byteImage);
+            inputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        this.encodedImage = Base64.getEncoder().encodeToString(byteImage);
     }
 }
