8c1e03bf65d7b4ff8c3c6ef106de239513b4d492
EunChanNam <namhm23@gmail.com>
2023-04-13 20:58:19 +0900

    [Feat] 바이트데이터 응답을 Uri 응답으로 수정

--- src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
@@ -3,33 +3,26 @@ package com.inq.wishhair.wesharewishhair.photo.dto.response;
 import com.inq.wishhair.wesharewishhair.photo.domain.Photo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.core.io.UrlResource;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Base64;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
 
 @Getter
 @AllArgsConstructor
 public class PhotoResponse {
 
-    private final String encodedImage;
+    private final String resource;
 
     public PhotoResponse(Photo photo) {
-        String filePath = FilePath.VALUE;
-        File image = new File(filePath + photo.getStoreFilename());
-
-        byte[] byteImage = new byte[(int) image.length()];
+        String fullPath = FilePath.VALUE + photo.getStoreFilename();
+        Path path = Path.of(fullPath);
 
         try {
-            FileInputStream inputStream = new FileInputStream(image);
-            inputStream.read(byteImage);
-            inputStream.close();
-        } catch (IOException e) {
+            String stringUri = new UrlResource(path.toUri()).toString();
+            this.resource = stringUri.substring(5, stringUri.length() - 1);
+        } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
-
-        this.encodedImage = Base64.getEncoder().encodeToString(byteImage);
     }
 }
