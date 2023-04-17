0fcd6d46d5f9814a7571a3f49141b4aed6cd1d65
EunChanNam <namhm23@gmail.com>
2023-04-17 13:59:11 +0900

    [Feat] HairStyleServiceTest - 테스트 수정

--- src/test/java/com/inq/wishhair/wesharewishhair/hairstyle/service/HairStyleSearchServiceTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/hairstyle/service/HairStyleSearchServiceTest.java
@@ -30,10 +30,13 @@ public class HairStyleSearchServiceTest extends ServiceTest {
     private HairStyleSearchService hairStyleSearchService;
 
     private final HairStyle[] hairStyles = new HairStyle[values().length];
+    private User user;
 
     @BeforeEach
     void init() {
         //given
+        user = userRepository.save(UserFixture.B.toEntity());
+
         HairStyleFixture[] fixtures = values();
         for (int i = 0; i < values().length; i++) {
             hairStyles[i] = hairStyleSearchRepository.save(fixtures[i].toEntity());
@@ -44,15 +47,15 @@ public class HairStyleSearchServiceTest extends ServiceTest {
     @DisplayName("헤어스타일 추천 로직")
     class findRecommendedHairStyle {
         @Test
-        @DisplayName("입력된 태그의 얼굴형 태그가 포함되지 않은 헤어스타일은 조회되지 않는다")
+        @DisplayName("사용자의 얼굴형 태그가 포함되지 않은 헤어스타일은 조회되지 않는다")
         void test1() {
             //given
             List<Tag> tags = A.getTags();
-            Long userId = userRepository.save(UserFixture.B.toEntity()).getId();
+            user.updateFaceShape(new FaceShape(Tag.SQUARE));
 
             //when
             ResponseWrapper<HairStyleResponse> result =
-                    hairStyleSearchService.recommendHair(tags, userId);
+                    hairStyleSearchService.recommendHair(tags, user.getId());
 
             //then
             List<HairStyleResponse> actual = result.getResult();
@@ -60,15 +63,14 @@ public class HairStyleSearchServiceTest extends ServiceTest {
         }
 
         @Test
-        @DisplayName("입력된 태그에 얼굴형 태그가 포함되지 않으면 400 예외를 던진다")
+        @DisplayName("사용자가 얼굴형 태그가 없는 사용자면 403 예외를 던진다")
         void test2() {
             //given
             List<Tag> tags = List.of(Tag.PERM);
-            Long userId = userRepository.save(UserFixture.B.toEntity()).getId();
-            ErrorCode expectedError = ErrorCode.RUN_NO_FACE_SHAPE_TAG;
+            ErrorCode expectedError = ErrorCode.USER_NO_FACE_SHAPE_TAG;
 
             //when, then
-            assertThatThrownBy(() -> hairStyleSearchService.recommendHair(tags, userId))
+            assertThatThrownBy(() -> hairStyleSearchService.recommendHair(tags, user.getId()))
                     .isInstanceOf(WishHairException.class)
                     .hasMessageContaining(expectedError.getMessage());
         }
@@ -78,36 +80,15 @@ public class HairStyleSearchServiceTest extends ServiceTest {
         void test3() {
             //given
             List<Tag> tags = E.getTags();
-            Long userId = userRepository.save(UserFixture.B.toEntity()).getId();
+            user.updateFaceShape(new FaceShape(Tag.OBLONG));
 
             //when
             ResponseWrapper<HairStyleResponse> result =
-                    hairStyleSearchService.recommendHair(tags, userId);
+                    hairStyleSearchService.recommendHair(tags, user.getId());
 
             //then
             List<HairStyleResponse> actual = result.getResult();
             assertHairStyleResponseMatch(actual, List.of(4, 2, 3));
-        }
-
-        @Test
-        @DisplayName("헤어스타일을 추천 후 첫번째 헤어스타일의 얼굴형 태그로 사용자의 faceShapeTag 를 업데이트 한다")
-        void test4() {
-            //given
-            List<Tag> tags = E.getTags();
-            User user = UserFixture.B.toEntity();
-            userRepository.save(user);
-
-            //before
-            assertThat(user.existFaceShape()).isFalse();
-
-            //when
-            hairStyleSearchService.recommendHair(tags, user.getId());
-
-            //then
-            assertAll(
-                    () -> assertThat(user.existFaceShape()).isTrue(),
-                    () -> assertThat(user.getFaceShape()).isEqualTo(Tag.OBLONG) // <- E 헤어스타일의 얼굴형 태그
-            );
         }
     }
 
@@ -118,8 +99,6 @@ public class HairStyleSearchServiceTest extends ServiceTest {
         @DisplayName("얼굴형 태그가 저장돼 있는 유저라면 얼굴형 태그 기반으로 헤어가 찜수, 이름으로 정렬되어 조회된다")
         void test5() {
             //given
-            User user = UserFixture.B.toEntity();
-            userRepository.save(user);
             user.updateFaceShape(new FaceShape(Tag.OBLONG));
 
             //when
@@ -133,10 +112,6 @@ public class HairStyleSearchServiceTest extends ServiceTest {
         @Test
         @DisplayName("얼굴형 태그가 저장돼 있지 않은 유저라면 태그 없이 헤어가 찜수, 이름으로 정렬되어 조회된다")
         void test6() {
-            //given
-            User user = UserFixture.B.toEntity();
-            userRepository.save(user);
-
             //when
             ResponseWrapper<HairStyleResponse> result =
                     hairStyleSearchService.recommendHairByFaceShape(user.getId());
