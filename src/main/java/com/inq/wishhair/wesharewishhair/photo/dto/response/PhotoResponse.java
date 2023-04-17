2e29e4795b4e1bcf194985763e143205b183c370
EunChanNam <namhm23@gmail.com>
2023-04-17 12:17:12 +0900

    [Feat] PhotoResponse 응답 스펙 storeUrl 로 변경

--- src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/dto/response/PhotoResponse.java
@@ -3,26 +3,15 @@ package com.inq.wishhair.wesharewishhair.photo.dto.response;
 import com.inq.wishhair.wesharewishhair.photo.domain.Photo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.core.io.UrlResource;
 
-import java.net.MalformedURLException;
-import java.nio.file.Path;
 
 @Getter
 @AllArgsConstructor
 public class PhotoResponse {
 
-    private final String resource;
+    private final String storeUrl;
 
     public PhotoResponse(Photo photo) {
-        String fullPath = FilePath.VALUE + photo.getStoreUrl();
-        Path path = Path.of(fullPath);
-
-        try {
-            String stringUri = new UrlResource(path.toUri()).toString();
-            this.resource = stringUri.substring(5, stringUri.length() - 1);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
+        this.storeUrl = photo.getStoreUrl();
     }
 }
--- src/main/java/com/inq/wishhair/wesharewishhair/photo/utils/PhotoStore.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/utils/PhotoStore.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
