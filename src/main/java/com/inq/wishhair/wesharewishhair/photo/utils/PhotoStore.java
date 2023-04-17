ccc2e7192de796176dd9c4fe0f7c04610e525e70
EunChanNam <namhm23@gmail.com>
2023-04-17 11:27:34 +0900

    [Feat] PhotoStore 로직 수정

--- src/main/java/com/inq/wishhair/wesharewishhair/photo/utils/PhotoStore.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/photo/utils/PhotoStore.java
@@ -1,5 +1,9 @@
 package com.inq.wishhair.wesharewishhair.photo.utils;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
 import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
 import com.inq.wishhair.wesharewishhair.photo.domain.Photo;
@@ -9,44 +13,52 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.io.InputStream;
+import java.util.*;
 
 @Component
 public class PhotoStore {
 
-    @Value("${file.dir}")
-    private String fileDir;
+    private final AmazonS3Client amazonS3Client;
+    private final String buketName;
 
-    public String getFullPath(String filename) {
-        return fileDir + filename;
+    public PhotoStore(AmazonS3Client amazonS3Client,
+                      @Value("${cloud.aws.s3.bucket}") String buketName) {
+        this.amazonS3Client = amazonS3Client;
+        this.buketName = buketName;
     }
 
-    public List<Photo> storePhotos(List<MultipartFile> multipartFiles) {
-        if (multipartFiles == null) return new ArrayList<>(); // 사진을 저장하지 않는경우 null 방지
-        List<Photo> resultList = new ArrayList<>();
-        for (MultipartFile multipartFile : multipartFiles) {
-            Photo photo = storePhoto(multipartFile);
-            resultList.add(photo);
+    public Set<Photo> uploadFiles(List<MultipartFile> files) {
+        Set<Photo> photos = new HashSet<>();
+        files.forEach(file -> photos.add(uploadFile(file)));
+        return photos;
+    }
+
+    private Photo uploadFile(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new WishHairException(ErrorCode.EMPTY_FILE_EX);
         }
-        return resultList;
-    }
+        String originalFilename = file.getOriginalFilename();
+        String storeFilename = createStoreFilename(originalFilename);
 
-    private Photo storePhoto(MultipartFile multipartFile) {
-        if (!multipartFile.isEmpty()) {
-            String originalFilename = multipartFile.getOriginalFilename();
-            String storeFilename = createStoreFilename(originalFilename);
-            String fullPath = getFullPath(storeFilename);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
 
-            //IOException 잡아서 사용자 정의 Exception 으로 변경
-            try {
-                multipartFile.transferTo(new File(fullPath));
-            } catch (IOException e) {
-                throw new WishHairException(ErrorCode.FILE_TRANSFER_EX);
-            }
-            return Photo.of(originalFilename, storeFilename);
-        } else throw new WishHairException(ErrorCode.EMPTY_FILE_EX);
+        try (InputStream inputStream = file.getInputStream();) {
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                    buketName,
+                    storeFilename,
+                    inputStream,
+                    metadata).withCannedAcl(CannedAccessControlList.PublicRead);
+
+            amazonS3Client.putObject(putObjectRequest);
+
+            String storeUrl = amazonS3Client.getUrl(buketName, storeFilename).toString();
+            return Photo.of(originalFilename, storeUrl);
+        } catch (IOException e) {
+            throw new WishHairException(ErrorCode.FILE_TRANSFER_EX);
+        }
     }
 
     private String createStoreFilename(String originalFilename) {
