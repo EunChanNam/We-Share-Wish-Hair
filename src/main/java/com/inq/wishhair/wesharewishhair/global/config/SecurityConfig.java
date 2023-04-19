647d4b135482b75033e3a6059d51bbea06fc4b86
EunChanNam <namhm23@gmail.com>
2023-04-19 14:23:03 +0900

    [Feat] SecurityConfig 구현

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/global/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.inq.wishhair.wesharewishhair.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        return http.build();
+    }
+}
