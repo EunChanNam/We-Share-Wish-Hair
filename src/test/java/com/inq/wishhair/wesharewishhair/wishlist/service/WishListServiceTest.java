653d25c43743ed5532d632b6285a9560ddc3375e
EunChanNam <namhm23@gmail.com>
2023-04-19 19:44:08 +0900

    [Test] 찜목록 삭제 서비스 테스트

--- src/test/java/com/inq/wishhair/wesharewishhair/wishlist/service/WishListServiceTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/wishlist/service/WishListServiceTest.java
@@ -1,18 +1,18 @@
 package com.inq.wishhair.wesharewishhair.wishlist.service;
 
 import com.inq.wishhair.wesharewishhair.global.base.ServiceTest;
+import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
 import com.inq.wishhair.wesharewishhair.global.fixture.HairStyleFixture;
 import com.inq.wishhair.wesharewishhair.global.fixture.UserFixture;
 import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyle;
 import com.inq.wishhair.wesharewishhair.user.domain.User;
 import com.inq.wishhair.wesharewishhair.wishlist.domain.WishList;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("WishListServiceTest - SpringBootTest")
@@ -46,5 +46,34 @@ public class WishListServiceTest extends ServiceTest {
                     assertThat(actual.getHairStyle()).isEqualTo(hairStyle);
                 }
         );
+    }
+
+    @Nested
+    @DisplayName("찜목록 삭제 서비스 테스트")
+    class deleteWishList {
+        @Test
+        @DisplayName("찜목록을 삭제한다")
+        void success() {
+            //given
+            Long wishListId = wishListRepository.save(WishList.createWishList(user, hairStyle)).getId();
+
+            //when
+            wishListService.deleteWishList(wishListId, user.getId());
+
+            //then
+            assertThat(wishListRepository.findAll()).isEmpty();
+        }
+
+        @Test
+        @DisplayName("찜 목록 소유자가 아니여서 실패한다")
+        void failByNotHost() {
+            //given
+            Long wishListId = wishListRepository.save(WishList.createWishList(user, hairStyle)).getId();
+
+            //when, then
+            assertThatThrownBy(() -> wishListService.deleteWishList(wishListId, 99L))
+                    .isInstanceOf(WishHairException.class)
+                    .hasMessageContaining(ErrorCode.WISH_LIST_NOT_HOST.getMessage());
+        }
     }
 }
