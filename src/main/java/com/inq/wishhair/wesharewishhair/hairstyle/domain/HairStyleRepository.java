2f9d9ba55c06051bf1d3acf4bec56f4a87ad1957
EunChanNam <namhm23@gmail.com>
2023-04-19 17:44:12 +0900

    [Feat] wishlist 를 생성 할때 발생하는 동시성 제어 코드 추가

--- src/main/java/com/inq/wishhair/wesharewishhair/hairstyle/domain/HairStyleRepository.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/hairstyle/domain/HairStyleRepository.java
@@ -1,6 +1,13 @@
 package com.inq.wishhair.wesharewishhair.hairstyle.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import javax.persistence.LockModeType;
+import java.util.Optional;
 
 public interface HairStyleRepository extends JpaRepository<HairStyle, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    Optional<HairStyle> findWithLockById(Long id);
 }
