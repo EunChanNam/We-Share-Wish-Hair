726a48dc7f3bd12207f94d90fb7dd279f674eeba
EunChanNam <namhm23@gmail.com>
2023-04-22 17:12:41 +0900

    [Feat] hairStyle query dsl 구현

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/hairstyle/infra/query/HairStyleQueryRepository.java
@@ -0,0 +1,17 @@
+package com.inq.wishhair.wesharewishhair.hairstyle.infra.query;
+
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyle;
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.hashtag.enums.Tag;
+import com.inq.wishhair.wesharewishhair.user.domain.FaceShape;
+import com.inq.wishhair.wesharewishhair.user.enums.Sex;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface HairStyleQueryRepository {
+
+    List<HairStyle> findByHashTags(List<Tag> tags, Sex sex, Pageable pageable);
+
+    List<HairStyle> findByFaceShapeTag(FaceShape faceShape, Sex sex, Pageable pageable);
+}
---
+++ src/main/java/com/inq/wishhair/wesharewishhair/hairstyle/infra/query/HairStyleQueryRepositoryImpl.java
@@ -0,0 +1,71 @@
+package com.inq.wishhair.wesharewishhair.hairstyle.infra.query;
+
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyle;
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.QHairStyle;
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.hashtag.QHashTag;
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.hashtag.enums.Tag;
+import com.inq.wishhair.wesharewishhair.review.domain.Review;
+import com.inq.wishhair.wesharewishhair.user.domain.FaceShape;
+import com.inq.wishhair.wesharewishhair.user.enums.Sex;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class HairStyleQueryRepositoryImpl implements HairStyleQueryRepository{
+
+    private final JPAQueryFactory factory;
+
+    private final QHairStyle hairStyle = new QHairStyle("h");
+    private final QHashTag hashTag = new QHashTag("t");
+
+    @Override
+    public List<HairStyle> findByHashTags(List<Tag> tags, Sex sex, Pageable pageable) {
+        return factory
+                .select(hairStyle)
+                .from(hairStyle)
+                .leftJoin(hairStyle.hashTags, hashTag)
+                .where(hashTag.tag.in(tags))
+                .where(hairStyle.sex.eq(sex))
+                .groupBy(hashTag.id)
+                .orderBy(mainQueryOrderBy().toArray(OrderSpecifier[]::new))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    @Override
+    public List<HairStyle> findByFaceShapeTag(FaceShape faceShape, Sex sex, Pageable pageable) {
+        JPAQuery<HairStyle> query = factory
+                .select(hairStyle)
+                .from(hairStyle)
+                .where(hairStyle.sex.eq(sex));
+
+        if (faceShape != null && faceShape.getTag() != null) {
+            query.leftJoin(hairStyle.hashTags, hashTag)
+                    .where(hashTag.tag.eq(faceShape.getTag()));
+        }
+
+        return query
+                .orderBy(hairStyle.wishListCount.value.desc(), hairStyle.name.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private List<OrderSpecifier<?>> mainQueryOrderBy() {
+        List<OrderSpecifier<?>> orderBy = new LinkedList<>();
+
+        orderBy.add(hairStyle.count().desc());
+        orderBy.add(hairStyle.wishListCount.value.desc());
+        orderBy.add(hairStyle.name.asc());
+        return orderBy;
+    }
+}
