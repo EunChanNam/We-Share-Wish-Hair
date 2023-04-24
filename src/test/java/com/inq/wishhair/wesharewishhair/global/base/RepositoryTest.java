2a319d482782c76c7a0448a9b7895a480595f770
EunChanNam <namhm23@gmail.com>
2023-04-24 16:37:22 +0900

    [Feat] LikeReviewQueryRepository 테스트 코드 작성

--- src/test/java/com/inq/wishhair/wesharewishhair/global/base/RepositoryTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/global/base/RepositoryTest.java
@@ -3,6 +3,7 @@ package com.inq.wishhair.wesharewishhair.global.base;
 import com.inq.wishhair.wesharewishhair.global.config.JpaAuditingConfig;
 import com.inq.wishhair.wesharewishhair.global.config.QueryDslConfig;
 import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyleRepository;
+import com.inq.wishhair.wesharewishhair.review.domain.ReviewRepository;
 import com.inq.wishhair.wesharewishhair.user.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -25,6 +26,9 @@ public abstract class RepositoryTest {
 
     @Autowired
     protected HairStyleRepository hairStyleRepository;
+
+    @Autowired
+    protected ReviewRepository reviewRepository;
 
     @PersistenceContext
     protected EntityManager em;
