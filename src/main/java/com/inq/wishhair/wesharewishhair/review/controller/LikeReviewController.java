97fc2e063417750c10cf775f6fe7273e304d0b57
EunChanNam <namhm23@gmail.com>
2023-04-24 16:28:48 +0900

    [Feat] likeReviewControlelr - API 구현

--- src/main/java/com/inq/wishhair/wesharewishhair/review/controller/LikeReviewController.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/review/controller/LikeReviewController.java
@@ -3,26 +3,32 @@ package com.inq.wishhair.wesharewishhair.review.controller;
 import com.inq.wishhair.wesharewishhair.auth.config.resolver.ExtractPayload;
 import com.inq.wishhair.wesharewishhair.global.dto.response.Success;
 import com.inq.wishhair.wesharewishhair.review.service.LikeReviewService;
+import com.inq.wishhair.wesharewishhair.review.service.dto.response.ReviewLikeResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/review/like/")
 public class LikeReviewController {
 
     private final LikeReviewService likeReviewService;
 
-    @PostMapping("/review/like/{reviewId}")
+    @PostMapping("/{reviewId}")
     public ResponseEntity<Success> likeReview(
             @PathVariable Long reviewId,
             @ExtractPayload Long userId) {
 
         likeReviewService.likeReview(reviewId, userId);
         return ResponseEntity.ok(new Success());
+    }
+
+    @GetMapping("/{reviewId}")
+    public ResponseEntity<ReviewLikeResponse> checkIsLiking(@ExtractPayload Long userId,
+                                                       @PathVariable Long reviewId) {
+
+        boolean result = likeReviewService.checkIsLiking(userId, reviewId);
+        return ResponseEntity.ok(new ReviewLikeResponse(result));
     }
 }
