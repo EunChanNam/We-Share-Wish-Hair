182cbafde49819bd114b2033873ed49d23cf9def
EunChanNam <namhm23@gmail.com>
2023-04-27 14:22:57 +0900

    [refactor] likeReviewService 수정

--- src/test/java/com/inq/wishhair/wesharewishhair/review/service/LikeReviewServiceTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/review/service/LikeReviewServiceTest.java
@@ -1,18 +1,17 @@
 package com.inq.wishhair.wesharewishhair.review.service;
 
-import com.inq.wishhair.wesharewishhair.global.fixture.ReviewFixture;
-import com.inq.wishhair.wesharewishhair.global.fixture.UserFixture;
 import com.inq.wishhair.wesharewishhair.global.base.ServiceTest;
-import com.inq.wishhair.wesharewishhair.review.domain.Review;
+import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
+import com.inq.wishhair.wesharewishhair.hairstyle.domain.wishhair.WishHair;
 import com.inq.wishhair.wesharewishhair.review.domain.likereview.LikeReview;
-import com.inq.wishhair.wesharewishhair.user.domain.User;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("Review-LikeReviewServiceTest - SpringBootTest")
 public class LikeReviewServiceTest extends ServiceTest {
@@ -20,51 +19,60 @@ public class LikeReviewServiceTest extends ServiceTest {
     @Autowired
     private LikeReviewService likeReviewService;
 
-    private User user;
-    private Review review;
-
-    @BeforeEach
-    void setUp() {
-        //given
-        user = userRepository.save(UserFixture.B.toEntity());
-        review = reviewRepository.save(ReviewFixture.A.toEntity(user, null));
-    }
+    private final Long userId = 1L;
+    private final Long reviewId = 1L;
 
     @Nested
     @DisplayName("좋아요를 실행한다")
-    class likeReview {
+    class executeLike {
         @Test
-        @DisplayName("좋아요를 하지 않은 리뷰에 좋아요를 실행해 좋아요가 생성된다")
-        void likeReview1() {
+        @DisplayName("좋아요를 생성한다")
+        void success() {
             //when
-            likeReviewService.executeLike(review.getId(), user.getId());
+            likeReviewService.executeLike(reviewId, userId);
 
             //then
             List<LikeReview> all = likeReviewRepository.findAll();
-            assertAll(
-                    () -> assertThat(review.getLikeReviews()).hasSize(1),
-                    () -> assertThat(review.getLikes()).isEqualTo(1),
-                    () -> assertThat(all).hasSize(1)
-            );
+            assertThat(all).hasSize(1);
         }
 
         @Test
-        @DisplayName("좋아요를 한 리뷰에 좋아요를 실행해 좋아요가 취소된다")
-        void likeReview2() {
+        @DisplayName("이미 좋아요 한 리뷰에 좋아요를 실행해 실패한다")
+        void fail() {
             //given
-            likeReviewService.executeLike(review.getId(), user.getId());
+            likeReviewService.executeLike(reviewId, userId);
+
+            //when, then
+            assertThatThrownBy(() -> likeReviewService.executeLike(reviewId, userId))
+                    .isInstanceOf(WishHairException.class)
+                    .hasMessageContaining(ErrorCode.REVIEW_ALREADY_LIKING.getMessage());
+        }
+    }
+
+    @Nested
+    @DisplayName("좋아요를 취소한다")
+    class cancelLike {
+        @Test
+        @DisplayName("좋아요를 취소한다")
+        void success() {
+            //given
+            likeReviewService.executeLike(reviewId, userId);
 
             //when
-            em.flush();
-            likeReviewService.executeLike(review.getId(), user.getId());
+            likeReviewService.cancelLike(reviewId, userId);
 
             //then
             List<LikeReview> all = likeReviewRepository.findAll();
-            assertAll(
-                    () -> assertThat(review.getLikeReviews()).isEmpty(),
-                    () -> assertThat(review.getLikes()).isZero(),
-                    () -> assertThat(all).isEmpty()
-            );
+            assertThat(all).isEmpty();
+        }
+
+        @Test
+        @DisplayName("좋아요 하지 않은 리뷰에 좋아요 취소를 실행해 실패한다")
+        void fail() {
+            //when, then
+            assertThatThrownBy(() -> likeReviewService.cancelLike(reviewId, userId))
+                    .isInstanceOf(WishHairException.class)
+                    .hasMessageContaining(ErrorCode.REVIEW_NOT_LIKING.getMessage());
         }
     }
 
@@ -75,10 +83,10 @@ public class LikeReviewServiceTest extends ServiceTest {
         @DisplayName("좋아요가 존재하여 true 를 응답한다")
         void exist() {
             //given
-            likeReviewRepository.save(LikeReview.addLike(user, review));
+            likeReviewRepository.save(LikeReview.addLike(userId, reviewId));
 
             //when
-            boolean result = likeReviewService.checkIsLiking(user.getId(), review.getId());
+            boolean result = likeReviewService.checkIsLiking(userId, reviewId);
 
             //then
             assertThat(result).isTrue();
@@ -88,7 +96,7 @@ public class LikeReviewServiceTest extends ServiceTest {
         @DisplayName("좋아요가 존재하지 않아 false 를 응답한다")
         void notExist() {
             //when
-            boolean result = likeReviewService.checkIsLiking(user.getId(), review.getId());
+            boolean result = likeReviewService.checkIsLiking(userId, reviewId);
 
             //then
             assertThat(result).isFalse();
