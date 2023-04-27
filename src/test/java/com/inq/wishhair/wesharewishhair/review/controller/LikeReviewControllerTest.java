726dc591b7d6af5825ac0dcdc5d9e9bfe154a9ac
EunChanNam <namhm23@gmail.com>
2023-04-27 18:04:25 +0900

    [refactor] likereviewcontrollertest - 테스트 코드 재작성

--- src/test/java/com/inq/wishhair/wesharewishhair/review/controller/LikeReviewControllerTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/review/controller/LikeReviewControllerTest.java
@@ -2,18 +2,18 @@ package com.inq.wishhair.wesharewishhair.review.controller;
 
 import com.inq.wishhair.wesharewishhair.global.base.ControllerTest;
 import com.inq.wishhair.wesharewishhair.global.exception.ErrorCode;
+import com.inq.wishhair.wesharewishhair.global.exception.WishHairException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static com.inq.wishhair.wesharewishhair.global.utils.TokenUtils.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
@@ -28,7 +28,7 @@ public class LikeReviewControllerTest extends ControllerTest {
 
     @Nested
     @DisplayName("좋아요 API 테스트")
-    class likeReview {
+    class executeLike {
         @Test
         @DisplayName("헤더에 Access 토큰을 포함하지 않아서 예외를 던진다")
         void failByNoAccessToken() throws Exception {
@@ -61,12 +61,83 @@ public class LikeReviewControllerTest extends ControllerTest {
                             restDocs.document(
                                     accessTokenHeaderDocument(),
                                     pathParameters(
-                                            parameterWithName("reviewId").attributes(constraint("필수 입니다"))
-                                            .description("좋아요 할 리뷰 ID(PK)")
-                                            ),
+                                            parameterWithName("reviewId").description("좋아요 할 리뷰 ID(PK)")
+                                    ),
                                     successResponseDocument()
                             )
                     );
+        }
+
+        @Test
+        @DisplayName("좋아요 돼있는 리뷰에 좋아요를 실행해 실패한다")
+        void failByAlreadyExistLike() throws Exception {
+            //given
+            ErrorCode expectedError = ErrorCode.REVIEW_ALREADY_LIKING;
+            doThrow(new WishHairException(expectedError)).when(likeReviewService).executeLike(any(), any());
+
+            //when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .post(BASE_URL + "/{reviewId}", 1L)
+                    .header(AUTHORIZATION, BEARER + ACCESS_TOKEN);
+
+            //then
+            assertException(expectedError, requestBuilder, status().isBadRequest());
+        }
+    }
+
+    @Nested
+    @DisplayName("좋아요 취소 API 테스트")
+    class cancelLike {
+        @Test
+        @DisplayName("좋아요를 취소한다")
+        void success() throws Exception {
+            //when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .post(BASE_URL + "/cancel/{reviewId}", 1L)
+                    .header(AUTHORIZATION, BEARER + ACCESS_TOKEN);
+
+            //then
+            mockMvc.perform(requestBuilder)
+                    .andExpect(status().isOk())
+                    .andDo(
+                            restDocs.document(
+                                    accessTokenHeaderDocument(),
+                                    pathParameters(
+                                            parameterWithName("reviewId").description("좋아요 취소 할 리뷰 ID(PK)")
+                                    ),
+                                    successResponseDocument()
+                            )
+                    );
+        }
+
+        @Test
+        @DisplayName("헤더에 토큰을 포함하지 않아 실패한다")
+        void failByNoAccessToken() throws Exception {
+            //given
+            ErrorCode expectedError = ErrorCode.AUTH_REQUIRED_LOGIN;
+
+            //when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .post(BASE_URL + "/cancel/{reviewId}", 1L);
+
+            //then
+            assertException(expectedError, requestBuilder, status().isUnauthorized());
+        }
+
+        @Test
+        @DisplayName("좋아요 되있지 않은 리뷰에 좋아요 취소를 실행해 실패한다")
+        void failByLikeIsNotExist() throws Exception {
+            //given
+            ErrorCode expectedError = ErrorCode.REVIEW_NOT_LIKING;
+            doThrow(new WishHairException(expectedError)).when(likeReviewService).cancelLike(any(), any());
+
+            //when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .post(BASE_URL + "/cancel/{reviewId}", 1L)
+                    .header(AUTHORIZATION, BEARER + ACCESS_TOKEN);
+
+            //then
+            assertException(expectedError, requestBuilder, status().isBadRequest());
         }
     }
 
