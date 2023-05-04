702e4a3d80acdb559221626ea8fa7249455d9b23
EunChanNam <namhm23@gmail.com>
2023-05-04 14:01:05 +0900

    [feat] SimpleHairStyleResponse 구현

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/hairstyle/service/dto/response/HairStyleSimpleResponse.java
@@ -0,0 +1,12 @@
+package com.inq.wishhair.wesharewishhair.hairstyle.service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class HairStyleSimpleResponse {
+
+    private Long hairStyleId;
+    private String hairStyleName;
+}
