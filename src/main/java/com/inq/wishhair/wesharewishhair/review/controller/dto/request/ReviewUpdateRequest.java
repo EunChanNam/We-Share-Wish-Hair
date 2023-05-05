11776ac42e853481b08efb97b8dac8ed00ac6113
EunChanNam <namhm23@gmail.com>
2023-05-05 10:05:41 +0900

    [feat] ReviewUpdateRequest 구현

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/review/controller/dto/request/ReviewUpdateRequest.java
@@ -0,0 +1,25 @@
+package com.inq.wishhair.wesharewishhair.review.controller.dto.request;
+
+import com.inq.wishhair.wesharewishhair.review.domain.enums.Score;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReviewUpdateRequest {
+
+    @NotNull
+    private String contents;
+
+    @NotNull
+    private Score score;
+
+    private List<MultipartFile> files;
+}
