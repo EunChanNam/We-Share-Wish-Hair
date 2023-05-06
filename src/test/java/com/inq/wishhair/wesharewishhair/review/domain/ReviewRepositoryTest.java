5f3d19e6a5549051fd05be35e5252b7f6702c6e0
EunChanNam <namhm23@gmail.com>
2023-05-06 12:05:29 +0900

    [test] ReviewRepositoryTest - 작성자 아이디로 사진 정보와 함께 조회한다

--- src/test/java/com/inq/wishhair/wesharewishhair/review/domain/ReviewRepositoryTest.java
+++ src/test/java/com/inq/wishhair/wesharewishhair/review/domain/ReviewRepositoryTest.java
@@ -1,56 +1,51 @@
 package com.inq.wishhair.wesharewishhair.review.domain;
 
 import com.inq.wishhair.wesharewishhair.global.base.RepositoryTest;
-import com.inq.wishhair.wesharewishhair.global.fixture.HairStyleFixture;
-import com.inq.wishhair.wesharewishhair.global.fixture.ReviewFixture;
 import com.inq.wishhair.wesharewishhair.global.fixture.UserFixture;
-import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyle;
-import com.inq.wishhair.wesharewishhair.hairstyle.domain.HairStyleRepository;
 import com.inq.wishhair.wesharewishhair.user.domain.User;
-import com.inq.wishhair.wesharewishhair.user.domain.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Optional;
+import java.util.Arrays;
+import java.util.List;
 
+import static com.inq.wishhair.wesharewishhair.global.fixture.ReviewFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("ReviewRepositoryTest - DataJpaTest")
 public class ReviewRepositoryTest extends RepositoryTest {
 
-    @Autowired
-    private ReviewRepository reviewRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private HairStyleRepository hairStyleRepository;
-
     private User user;
-    private Review review;
+    private final Review[] reviews = new Review[values().length];
 
     @BeforeEach
     void setUp() {
         //given
         user = userRepository.save(UserFixture.A.toEntity());
-        HairStyle hairStyle = hairStyleRepository.save(HairStyleFixture.A.toEntity());
-        review = reviewRepository.save(ReviewFixture.A.toEntity(user, hairStyle));
+        for (int i = 0; i < values().length; i++) {
+            reviews[i] = reviewRepository.save(values()[i].toEntity(user, null));
+
+        }
     }
 
     @Test
-    @DisplayName("리뷰를 아이디로 유저 정보와 함께 조회한다")
-    void findWithUserById() {
+    @DisplayName("리뷰를 아이디로 사진 정보와 함께 조회한다")
+    void findWithPhotosById() {
         //when
-        Review result = reviewRepository.findWithPhotosById(review.getId()).orElseThrow();
+        Review result = reviewRepository.findWithPhotosById(reviews[0].getId()).orElseThrow();
 
         //then
-        assertAll(
-                () -> assertThat(result).isEqualTo(review),
-                () -> assertThat(result.getWriter()).isEqualTo(user)
-        );
+        assertThat(result).isEqualTo(reviews[0]);
+    }
+
+    @Test
+    @DisplayName("작성자 아이디로 사진 정보와 함께 조회한다")
+    void findWithPhotosByUserId() {
+        //when
+        List<Review> result = reviewRepository.findWithPhotosByUserId(user.getId());
+
+        //then
+        assertThat(result).containsAll(Arrays.asList(reviews));
     }
 }
