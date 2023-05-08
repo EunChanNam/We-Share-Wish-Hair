d85074851fc7b322a86cf0820a3363207fa02c8d
EunChanNam <namhm23@gmail.com>
2023-05-08 20:49:09 +0900

    [feat] Google Response 구현

---
+++ src/main/java/com/inq/wishhair/wesharewishhair/auth/infra/oauth/dto/response/GoogleUserResponse.java
@@ -0,0 +1,15 @@
+package com.inq.wishhair.wesharewishhair.auth.infra.oauth.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoogleUserResponse {
+
+    private String name;
+    private String email;
+}
