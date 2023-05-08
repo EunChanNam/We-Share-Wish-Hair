525039f61c3cecd735fddc6a1e671fe606e44d13
EunChanNam <namhm23@gmail.com>
2023-05-08 21:19:42 +0900

    [feat] OAuth access API 구현

--- src/main/java/com/inq/wishhair/wesharewishhair/auth/controller/AuthController.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/auth/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.inq.wishhair.wesharewishhair.auth.controller;
 
 import com.inq.wishhair.wesharewishhair.auth.config.resolver.ExtractPayload;
+import com.inq.wishhair.wesharewishhair.auth.infra.oauth.OAuthUriGenerator;
 import com.inq.wishhair.wesharewishhair.auth.service.AuthService;
 import com.inq.wishhair.wesharewishhair.auth.service.dto.response.TokenResponse;
 import com.inq.wishhair.wesharewishhair.auth.controller.dto.request.LoginRequest;
+import com.inq.wishhair.wesharewishhair.global.dto.response.SimpleResponseWrapper;
 import com.inq.wishhair.wesharewishhair.global.dto.response.Success;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,20 +17,23 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final OAuthUriGenerator uriGenerator;
+
+    @GetMapping("/access")
+    public ResponseEntity<SimpleResponseWrapper<String>> access() {
+        String link = uriGenerator.generate();
+        return ResponseEntity.ok(new SimpleResponseWrapper<>(link));
+    }
 
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest loginRequest) {
-
         TokenResponse response = authService.login(loginRequest.getEmail(), loginRequest.getPw());
-
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/logout")
     public ResponseEntity<Success> logout(@ExtractPayload Long userId) {
-
         authService.logout(userId);
-
         return ResponseEntity.ok(new Success());
     }
 }
