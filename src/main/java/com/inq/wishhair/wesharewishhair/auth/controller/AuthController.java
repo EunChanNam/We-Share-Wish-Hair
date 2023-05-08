bbc03dd8a062b5e7a0dc6dce95c3b9d4ad645036
EunChanNam <namhm23@gmail.com>
2023-05-08 22:00:49 +0900

    [refactor] OAuth 코드 분리

--- src/main/java/com/inq/wishhair/wesharewishhair/auth/controller/AuthController.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/auth/controller/AuthController.java
@@ -1,11 +1,9 @@
 package com.inq.wishhair.wesharewishhair.auth.controller;
 
 import com.inq.wishhair.wesharewishhair.auth.config.resolver.ExtractPayload;
-import com.inq.wishhair.wesharewishhair.auth.infra.oauth.OAuthUriGenerator;
 import com.inq.wishhair.wesharewishhair.auth.service.AuthService;
 import com.inq.wishhair.wesharewishhair.auth.service.dto.response.TokenResponse;
 import com.inq.wishhair.wesharewishhair.auth.controller.dto.request.LoginRequest;
-import com.inq.wishhair.wesharewishhair.global.dto.response.SimpleResponseWrapper;
 import com.inq.wishhair.wesharewishhair.global.dto.response.Success;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +15,6 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
-    private final OAuthUriGenerator uriGenerator;
-
-    @GetMapping("/access")
-    public ResponseEntity<SimpleResponseWrapper<String>> access() {
-        String link = uriGenerator.generate();
-        return ResponseEntity.ok(new SimpleResponseWrapper<>(link));
-    }
 
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest loginRequest) {
