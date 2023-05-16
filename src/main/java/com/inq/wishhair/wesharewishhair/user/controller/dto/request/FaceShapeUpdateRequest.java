52cf2e7353f4110cc597f9d4c54c4595b0517fa7
EunChanNam <namhm23@gmail.com>
2023-05-16 10:09:45 +0900

    [feat] FaceShapeUpdateRequest 스펙 얼굴형 태그에서 file 로 변경

--- src/main/java/com/inq/wishhair/wesharewishhair/user/controller/dto/request/FaceShapeUpdateRequest.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/user/controller/dto/request/FaceShapeUpdateRequest.java
@@ -1,15 +1,15 @@
 package com.inq.wishhair.wesharewishhair.user.controller.dto.request;
 
-import com.inq.wishhair.wesharewishhair.hairstyle.domain.hashtag.enums.Tag;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class FaceShapeUpdateRequest {
 
-    private Tag faceShapeTag;
+    private MultipartFile file;
 }
