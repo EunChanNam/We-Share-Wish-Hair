f6aca0f69490e60e49eeb1cae4c06462f5ad5bef
EunChanNam <namhm23@gmail.com>
2023-06-19 15:19:50 +0900

    refactor: 불필요한 어노테이션 제거

--- src/main/java/com/inq/wishhair/wesharewishhair/auth/domain/TokenRepository.java
+++ src/main/java/com/inq/wishhair/wesharewishhair/auth/domain/TokenRepository.java
@@ -5,11 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
     Optional<Token> findByUser(User user);
